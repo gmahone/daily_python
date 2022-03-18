7c49b8ac8e64ef60ed167f44aff41f5bc09f42a0
gmahone <gmahone@gmail.com>
2022-03-18 22:03:46 +0100

    convert str to int for return

--- convert_num_to_reverse_array.py
+++ convert_num_to_reverse_array.py
@@ -1,7 +1,7 @@
 def reverse_array(n):
     out = [None] * len(str(n))
     for i in range(1, len(str(n))+1):
-        out[i-1] = str(n)[-i]
+        out[i-1] = int(str(n)[-i])
     return print(out)
 
 
