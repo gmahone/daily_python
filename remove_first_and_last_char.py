2e48a263ee5363557ab73132392e9699c836379e
gmahone <gmahone@gmail.com>
2022-03-21 22:07:49 +0100

    added other solutions

--- remove_first_and_last_char.py
+++ remove_first_and_last_char.py
@@ -3,3 +3,16 @@ def remove_first_and_last_char(x):
 
 test = "title"
 print(remove_first_and_last_char(test))
+
+
+# other solutions
+def remove_char(s):
+    return s[1 : -1]
+
+remove_char=lambda s: s[1:-1]
+
+def remove_char(s):
+    s = list(s)
+    s.pop()
+    s.pop(0)
+    return ''.join(s)
