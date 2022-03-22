bd412f3f4aa9451b635b11a0464d3356b2e0648c
gmahone <gmahone@gmail.com>
2022-03-22 21:54:49 +0100

    add other solutions

--- directions_reduce.py
+++ directions_reduce.py
@@ -29,3 +29,43 @@ def dirReduc(arr):
 print(reduce_directions(["East", "West", "North", "North", "South"]))
 
 print(reduce_directions(["East", "North", "West", "South"]))
+
+# other solutions
+
+opposite = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'}
+
+def dirReduc(plan):
+    new_plan = []
+    for d in plan:
+        if new_plan and new_plan[-1] == opposite[d]:
+            new_plan.pop()
+        else:
+            new_plan.append(d)
+    return new_plan
+
+def dirReduc(arr):
+    dir = " ".join(arr)
+    dir2 = dir.replace("NORTH SOUTH",'').replace("SOUTH NORTH",'').replace("EAST WEST",'').replace("WEST EAST",'')
+    dir3 = dir2.split()
+    return dirReduc(dir3) if len(dir3) < len(arr) else dir3
+
+def dirReduc(arr):
+    opposites = [{'NORTH', 'SOUTH'}, {'EAST', 'WEST'}]
+    
+    for i in range(len(arr)-1):
+        if set(arr[i:i+2]) in opposites:
+            del arr[i:i+2]
+            return dirReduc(arr)
+    
+    return arr 
+
+def dirReduc(arr):
+    i = 1
+    while i < len(arr) and len(arr) > 1:
+        if sorted([arr[i], arr[i-1]]) in [["NORTH", "SOUTH"], ["EAST", "WEST"]]:
+            del arr[i-1:i+1]
+            i = 1
+        else:
+            i += 1
+    return arr
+
