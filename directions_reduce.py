f6eceaff4e937fbcab63ca6307eda6899be09bfd
gmahone <gmahone@gmail.com>
2022-03-22 21:58:00 +0100

    add other solution

--- directions_reduce.py
+++ directions_reduce.py
@@ -69,3 +69,18 @@ def dirReduc(arr):
             i += 1
     return arr
 
+opposite = {"NORTH": "SOUTH", "SOUTH": "NORTH", "EAST": "WEST", "WEST": "EAST"}
+
+def dirReduc(arr):
+    print(arr)
+    i = 0
+    while i < len(arr)-1:
+        current = arr[i]
+        next = arr[i+1]
+        if next == opposite[current]:
+            arr.pop(i)
+            arr.pop(i)
+            i = 0
+        else:
+            i += 1
+    return arr
