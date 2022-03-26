a2d3e0b7d09593eee49450995278b22830bd036f
gmahone <gmahone@gmail.com>
2022-03-26 18:30:15 +0100

    add other solutions

--- deodorant_evaporator.py
+++ deodorant_evaporator.py
@@ -5,3 +5,8 @@ def evaporator(content, evap_per_day, threshold):
         content -= content * evap_per_day/100
         i += 1
     return i
+
+# other solutions
+import math
+def evaporator(content, evap_per_day, threshold):
+    return int(math.ceil(math.log(threshold/100.0, 1.0-evap_per_day/100.0)))
