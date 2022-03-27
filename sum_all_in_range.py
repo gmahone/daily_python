f0d8a2f9af48f6ba2eaa9683ca35856cc94c4df0
gmahone <gmahone@gmail.com>
2022-03-27 16:11:50 +0200

    add other solutions

--- sum_all_in_range.py
+++ sum_all_in_range.py
@@ -6,3 +6,11 @@ print(sum_all_in_range(1,4))
 print(sum_all_in_range(4,1))
 print(sum_all_in_range(-3,3))
 print(sum_all_in_range(4,4))
+
+
+# other solutions
+
+def get_sum(a,b):
+    return sum(xrange(min(a,b), max(a,b)+1))
+def get_sum(a, b):
+    return (a + b) * (abs(a - b) + 1) // 2
