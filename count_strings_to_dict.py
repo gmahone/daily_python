bf82cc3b5a3ab4f9f03aada0529bc625ecc5ff39
gmahone <gmahone@gmail.com>
2022-03-28 22:10:04 +0200

    add other solutions

--- count_strings_to_dict.py
+++ count_strings_to_dict.py
@@ -3,3 +3,11 @@ def count(string):
     for i in string:
         counts[i] = counts.get(i,0) + 1
     return counts
+
+# add other solutions
+
+def count(string):
+    return {i: string.count(i) for i in string}
+
+def count(s):
+    return {x:s.count(x) for x in set(s)}
