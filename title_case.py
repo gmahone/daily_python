8ceeef5b3762a92beae75651de4ca534ed76c92a
gmahone <gmahone@gmail.com>
2022-03-31 22:27:18 +0200

    add other solutions

--- title_case.py
+++ title_case.py
@@ -10,3 +10,22 @@ def title_case(title, minor_words=''):
         else:
             out.append(title_lower[i].capitalize())
     return " ".join(out)
+
+
+# other solutions
+
+def title_case(title, minor_words=''):
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join([word if word in minor_words else word.capitalize() for word in title])
+
+
+def title_case(title, minor_words=''):
+    title, minor_words = title.lower().split(), minor_words.lower().split()
+    for i in range(len(title)):
+        if i == 0 or title[i] not in minor_words:
+            title[i] = title[i].capitalize()
+    return ' '.join(title)
+
+def title_case(title, minor_words=''):
+    return ' '.join(w if w in minor_words.lower().split() and i else w.capitalize() for i, w in enumerate(title.lower().split()))
