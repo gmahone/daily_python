7133912ef634eff32b76bf75677b10cb24574853
gmahone <gmahone@gmail.com>
2022-04-02 22:41:20 +0200

    add other solutions

--- unique_in_order.py
+++ unique_in_order.py
@@ -10,3 +10,38 @@ def unique_in_order(iterable):
         if iterable[x] != iterable[x-1]:
             out.append(iterable[x])
     return out
+
+# other solutions
+
+def unique_in_order(iterable):
+    result = []
+    prev = None
+    for char in iterable[0:]:
+        if char != prev:
+            result.append(char)
+            prev = char
+    return result
+
+
+from itertools import groupby
+
+def unique_in_order(iterable):
+    return [k for (k, _) in groupby(iterable)]
+
+def unique_in_order(iterable):
+    res = []
+    for item in iterable:
+        if len(res) == 0 or item != res[-1]:
+            res.append(item)
+    return res
+
+unique_in_order = lambda l: [z for i, z in enumerate(l) if i == 0 or l[i - 1] != z]
+
+
+def unique_in_order(iterable):
+    r = []
+    for x in iterable:
+        x in r[-1:] or r.append(x)
+    return r
+
+
