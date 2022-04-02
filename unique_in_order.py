ff0b3d2a566684e8bcdf8514d150781d088b5877
gmahone <gmahone@gmail.com>
2022-04-02 22:39:26 +0200

    fixed bug for integer inputs

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,3 +1,12 @@
-def unique_in_order(string):
-    out = string[0] + "".join("" if string[x] == string[x-1] else string[x] for x in range(1, len(string)))
-    return list(out)
+# def unique_in_order(string):
+#     out = string[0] + "".join("" if string[x] == string[x-1] else string[x] for x in range(1, len(string)))
+#     return list(out)
+
+def unique_in_order(iterable):
+    if not len(iterable):
+        return []
+    out = [iterable[0]]
+    for x in range(1, len(iterable)):
+        if iterable[x] != iterable[x-1]:
+            out.append(iterable[x])
+    return out
