b3378fe0d35e30e3ccaf8825b7267809ec26e25e
gmahone <gmahone@gmail.com>
2022-04-03 21:23:48 +0200

    Create buying_a_car.py

---
+++ buying_a_car.py
@@ -0,0 +1,11 @@
+def nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMonth):
+    i = 0
+    savedThisMonth = 0
+    while (startPriceOld + savedThisMonth) <= startPriceNew:
+        i += 1
+        if i % 2 == 0:
+            percentLossByMonth += 0.5
+        startPriceOld *= 1-(percentLossByMonth/100)
+        startPriceNew *= 1-(percentLossByMonth/100)
+        savedThisMonth += savingperMonth
+    return [i, round(startPriceOld + savedThisMonth - startPriceNew)]
