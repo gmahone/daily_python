9892dca168c269a3f3b48e3b3114c2c46350ed77
gmahone <gmahone@gmail.com>
2022-04-09 21:28:22 +0200

    add other solutions

--- count_smileys.py
+++ count_smileys.py
@@ -7,3 +7,29 @@ def count_smileys(arr):
         if len(tmp) > 0:
             result.append(tmp)
     return len(result)
+
+# add other solutions
+
+from re import findall
+def count_smileys(arr):
+    return len(list(findall(r"[:;][-~]?[)D]", " ".join(arr))))
+
+# very deliberate solution -  all possible combos tested
+# but for this solution, don't need to import
+def count_smileys(arr):
+    eyes = [":", ";"]
+    noses = ["", "-", "~"]
+    mouths = [")", "D"]
+    count = 0
+    for eye in eyes:
+        for nose in noses:
+            for mouth in mouths:
+                face = eye + nose + mouth
+                count += arr.count(face)
+    return count
+
+
+import re
+
+def count_smileys(arr):
+    return sum(1 for s in arr if re.match(r'\A[:;][-~]?[)D]\Z',s))
