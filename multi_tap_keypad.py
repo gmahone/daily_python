c32639ef15e0b2150ce5e1920a97ee94d2703bf5
gmahone <gmahone@gmail.com>
2022-04-10 16:35:36 +0200

    fixed for all keypad characters

--- multi_tap_keypad.py
+++ multi_tap_keypad.py
@@ -1,10 +1,13 @@
 def presses(phrase):
-    pad = ["1","ABC2","DEF3","GHI4","JKL5","MNO6","PQRS7","TUV8","WXYZ9"," 0"]
+    pad = ["1","*","#","ABC2","DEF3","GHI4","JKL5","MNO6","PQRS7","TUV8","WXYZ9"," 0"]
     type_dict = {}
     for i in pad:
         for j in range(0, len(i)):
            type_dict.update({i[j]: (j+1)})
     presses = 0
     for i in phrase:
-        presses += type_dict[i.upper()]
+        print(i)
+        if i.isalnum() or i == " " or i == "*" or i == "#":
+            print(i)
+            presses += type_dict[i.upper()]
     return presses
