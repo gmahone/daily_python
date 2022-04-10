8990a3afa79be3402cf6ccc27138db00453e98fe
gmahone <gmahone@gmail.com>
2022-04-10 16:37:43 +0200

    add other solutions

--- multi_tap_keypad.py
+++ multi_tap_keypad.py
@@ -11,3 +11,60 @@ def presses(phrase):
             print(i)
             presses += type_dict[i.upper()]
     return presses
+
+# add other solutions
+
+BUTTONS = [ '1',   'abc2',  'def3',
+          'ghi4',  'jkl5',  'mno6',
+          'pqrs7', 'tuv8', 'wxyz9',
+            '*',   ' 0',    '#'   ]
+def presses(phrase):
+    return sum(1 + button.find(c) for c in phrase.lower() for button in BUTTONS if c in button)
+
+
+buttons = [
+    '1',
+    'abc2',
+    'def3',
+    'ghi4',
+    'jkl5',
+    'mno6',
+    'pqrs7',
+    'tuv8',
+    'wxyz9',
+    '*',
+    ' 0',
+    '#'
+]
+def getPresses(c):
+    for button in buttons:
+        if c in button:
+            return button.index(c) + 1
+    return 0
+
+def presses(phrase):
+    return sum(getPresses(c) for c in phrase.lower())
+
+
+def presses(phrase):
+    x = 0
+    for letter in phrase:
+        if letter.lower() in list('1*#adgjmptw '): x+= 1
+        elif letter.lower() in list('0behknqux'): x+= 2
+        elif letter.lower() in list('cfilorvy'): x+= 3
+        elif letter.lower() in list('234568sz'): x+= 4
+        elif letter.lower() in list('79'): x+= 5
+    return x
+
+
+def presses(phrase):
+    numbers = ['*','#','1','ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0']
+    print(phrase)
+    phrase = phrase.upper()
+    result = 0
+    
+    for letter in phrase:
+        for n in numbers:
+            if letter in n:
+                result = result + n.index(letter) + 1
+    return result
