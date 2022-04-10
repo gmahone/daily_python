b550e809f4acb06601358f4846d31fd4d1970e9f
gmahone <gmahone@gmail.com>
2022-04-10 16:38:33 +0200

    add lambda solution

--- multi_tap_keypad.py
+++ multi_tap_keypad.py
@@ -68,3 +68,7 @@ def presses(phrase):
             if letter in n:
                 result = result + n.index(letter) + 1
     return result
+
+
+presses = lambda s: sum((c in b) * (1 + b.find(c)) for c in s.lower() for b in
+               '1,abc2,def3,ghi4,jkl5,mno6,pqrs7,tuv8,wxyz9,*, 0,#'.split(","))
