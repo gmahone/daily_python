5e58febd599efc64de7e03542ceb39762997ca71
gmahone <gmahone@gmail.com>
2022-06-02 21:20:07 +0200

    add initial solution

--- sort_array_by_string_length.py
+++ sort_array_by_string_length.py
@@ -1,18 +1,31 @@
-def sort_by_length(arr):
-    pass
-
-
-test = ["Eyes", "Glasses", "Monocles", "Telescopes"]
-tmp_list = []
-result = []
-for i in test:
-    tmp_list.append({"item" : i, "length": len(i)})
+# def sort_by_length(arr):
+#     pass
+#
+#
+# test = ["Eyes", "Glasses", "Monocles", "Telescopes"]
+# tmp_list = []
+# result = []
+# for i in test:
+#     tmp_list.append({"item" : i, "length": len(i)})
+#
+# def extr_length(x):
+#     return x["length"]
+#
+# tmp_list.sort(key=extr_length)
+# for i in tmp_list:
+#     result.append(i["item"])
+#
+# print(result)
 
 def extr_length(x):
     return x["length"]
 
-tmp_list.sort(key=extr_length)
-for i in tmp_list:
-    result.append(i["item"])
-
-print(result)
+def sort_by_length(arr):
+    tmp_list = []
+    result = []
+    for i in test:
+        tmp_list.append({"item" : i, "length": len(i)})
+    tmp_list.sort(key=extr_length)
+    for i in tmp_list:
+        result.append(i["item"])
+    return result
