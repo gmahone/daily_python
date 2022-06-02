d557bf8e0699677a4ad87365063d634c785c1752
gmahone <gmahone@gmail.com>
2022-06-02 21:16:31 +0200

    add testing functions

--- sort_array_by_string_length.py
+++ sort_array_by_string_length.py
@@ -1,2 +1,18 @@
 def sort_by_length(arr):
     pass
+
+
+test = ["Eyes", "Glasses", "Monocles", "Telescopes"]
+tmp_list = []
+result = []
+for i in test:
+    tmp_list.append({"item" : i, "length": len(i)})
+
+def extr_length(x):
+    return x["length"]
+
+tmp_list.sort(key=extr_length)
+for i in tmp_list:
+    result.append(i["item"])
+
+print(result)
