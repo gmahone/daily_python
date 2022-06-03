bb14aa5c404dcf7243d9a3981ee62c6642043730
gmahone <gmahone@gmail.com>
2022-06-03 14:42:05 +0200

    fixed bug in multiplication

--- diff_of_volumes_of_cuboids.py
+++ diff_of_volumes_of_cuboids.py
@@ -2,6 +2,6 @@ def find_difference(a, b):
     vol_a = 1
     vol_b = 1
     for i in range(0, len(a)):
-        vol_a *= vol_a * a[i]
-        vol_b *= vol_b * b[i]
+        vol_a *= a[i]
+        vol_b *= b[i]
     return abs(vol_a - vol_b)
