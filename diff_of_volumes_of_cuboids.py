583ba56be93216327f49ae67062f0e78de17c0c4
gmahone <gmahone@gmail.com>
2022-06-03 14:41:50 +0200

    fixing bug in loop

--- diff_of_volumes_of_cuboids.py
+++ diff_of_volumes_of_cuboids.py
@@ -1,7 +1,7 @@
 def find_difference(a, b):
     vol_a = 1
     vol_b = 1
-    for i in range(a):
+    for i in range(0, len(a)):
         vol_a *= vol_a * a[i]
         vol_b *= vol_b * b[i]
     return abs(vol_a - vol_b)
