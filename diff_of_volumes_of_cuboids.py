01257210f167f1b1e157a869ee19e6e2d2e7ea51
gmahone <gmahone@gmail.com>
2022-06-03 14:49:42 +0200

    add solution using zip

--- diff_of_volumes_of_cuboids.py
+++ diff_of_volumes_of_cuboids.py
@@ -5,3 +5,14 @@ def find_difference(a, b):
         vol_a *= a[i]
         vol_b *= b[i]
     return abs(vol_a - vol_b)
+
+
+# other solutions
+
+# solution using zip()
+def find_difference(a, b):
+    A = B = 1
+    for i, j in zip(a, b):
+        A *= i
+        B *= j
+    return abs(A - B)
