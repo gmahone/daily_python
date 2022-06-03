7878db2a79f0af186d3c62af532aeb244d4c7f5f
gmahone <gmahone@gmail.com>
2022-06-03 21:47:00 +0200

    add initial solution

--- sum_the_strings.py
+++ sum_the_strings.py
@@ -1,3 +1,8 @@
 def sum_str(a, b):
-    # happy coding !
-    pass
+    int_a = 0
+    int_b = 0
+    if a is not "":
+        int_a = int(a)
+    if b is not "":
+        int_b = int(b)
+    return str(int_a+int_b)
