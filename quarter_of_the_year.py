ee1645b67bc570afba46804fa113885f49f164e1
gmahone <gmahone@gmail.com>
2022-06-04 22:01:14 +0200

    add explicit solution using if statements

--- quarter_of_the_year.py
+++ quarter_of_the_year.py
@@ -2,3 +2,17 @@ import math
 
 def quarter_of(month):
     return math.ceil(month / 12 * 4)
+
+# other solutions
+
+# explicit solutions
+def quarter_of(month):
+    # your code here
+    if month in range(1, 4):
+        return 1
+    elif month in range(4, 7):
+        return 2
+    elif month in range(7, 10):
+        return 3
+    elif month in range(10, 13):
+        return 4
