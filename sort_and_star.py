6d612e59ed8d42ee4f5b3e74d25c434c376736df
gmahone <gmahone@gmail.com>
2022-06-06 16:25:07 +0200

    add simpler solution using join of min(array)

--- sort_and_star.py
+++ sort_and_star.py
@@ -7,3 +7,8 @@ def get_char_code(element):
 def two_sort(array):
     first_item = sorted(array, key = get_char_code)[0]
     return "***".join(list(first_item))
+
+
+# other solutions
+def two_sort(array):
+    return '***'.join(min(array))
