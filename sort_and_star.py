cf2cd9aced39b12b29c87e01d16170800185e64a
gmahone <gmahone@gmail.com>
2022-06-06 16:09:22 +0200

    working on initial function

--- sort_and_star.py
+++ sort_and_star.py
@@ -2,4 +2,5 @@ def get_char_code(element):
     return ord(element[0])
 
 def two_sort(array):
-    print(array.sort(key = get_char_code))
+    first_item = sorted(array, key = get_char_code)[0]
+    print(first_item)
