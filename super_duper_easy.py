db2573ec321d327a8014504b8165389578624caa
gmahone <gmahone@gmail.com>
2022-06-07 22:18:16 +0200

    solution testing str set equality

--- super_duper_easy.py
+++ super_duper_easy.py
@@ -9,3 +9,7 @@ def problem(a):
 # other formulation, using isinstance
 def problem(a):
     return "Error" if isinstance(a,str) else a*50+6
+
+# testing as string equality
+def problem(a):
+    return "Error" if a == str(a) else a * 50 + 6
