52c7ef456f3ea15755bc42a2a58b5f6888fef8f6
gmahone <gmahone@gmail.com>
2022-06-08 21:55:01 +0200

    add reverse formulation of ternary

--- keep_up_the_hoop.py
+++ keep_up_the_hoop.py
@@ -7,3 +7,6 @@ def hoop_count(n):
 
 def hoop_count(n):
     return "Great, now move on to tricks" if n >= 10 else "Keep at it until you get it"
+
+def hoopCount(n):
+    return "Keep at it until you get it" if n < 10 else "Great, now move on to tricks"
