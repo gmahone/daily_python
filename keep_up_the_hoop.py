039599b322610f5d2e8b34f93ea30bd9e16dcccf
gmahone <gmahone@gmail.com>
2022-06-08 21:55:47 +0200

    add if else function solution

--- keep_up_the_hoop.py
+++ keep_up_the_hoop.py
@@ -4,9 +4,18 @@ def hoop_count(n):
         result = "Keep at it until you get it"
     return result
 
-
+# ternary
 def hoop_count(n):
     return "Great, now move on to tricks" if n >= 10 else "Keep at it until you get it"
 
+# reverse ternary
 def hoopCount(n):
     return "Keep at it until you get it" if n < 10 else "Great, now move on to tricks"
+
+
+# if else function
+def hoopCount(n):
+    if n >= 10:
+        return 'Great, now move on to tricks'
+    else:
+        return 'Keep at it until you get it'
