f524d9cf392e3abcc2febb7ed1e43d0d4a621e67
gmahone <gmahone@gmail.com>
2022-06-09 08:41:49 +0200

    add filter function

--- removing_elements.py
+++ removing_elements.py
@@ -1,3 +1,10 @@
+def filter_odd(input: [any]):
+    for i in range(0, len(input)):
+        if( i % 2 ):
+            return False
+        else:
+            return True
+
 def remove_every_other(my_list):
     # Your code here!
     pass
