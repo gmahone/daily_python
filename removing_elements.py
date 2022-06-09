c8d1f9550f3b686e3e8463c1d8866fc9f0a8e262
gmahone <gmahone@gmail.com>
2022-06-09 09:23:41 +0200

    add loop solution

--- removing_elements.py
+++ removing_elements.py
@@ -1,2 +1,12 @@
 def remove_every_other(my_list):
     return my_list[::2]
+
+# other solutions
+
+# loop based on range
+def remove_every_other(my_list):
+    r = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            r.append(my_list[i])
+    return r
