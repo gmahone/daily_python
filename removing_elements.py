9260e415166e2869ecba56ad20789ad2cbdaa24d
gmahone <gmahone@gmail.com>
2022-06-09 09:22:37 +0200

    change to simple initial solution

--- removing_elements.py
+++ removing_elements.py
@@ -1,12 +1,2 @@
-def filter_odd(input):
-    if( input % 2 ):
-        return False
-    else:
-        return True
-
 def remove_every_other(my_list):
-    filtered_list = filter(filter_odd, my_list)
-    result = []
-    for x in filtered_list:
-        result.append(x)
-    return result
+    return my_list[::2]
