bd10b663a37b381a07596d902011e4dc34e80c4f
gmahone <gmahone@gmail.com>
2022-06-12 22:31:32 +0200

    submission for initial solution

--- take_a_ten_minutes_walk.py
+++ take_a_ten_minutes_walk.py
@@ -1,7 +1,6 @@
 def is_valid_walk(walk):
-    # test if length > 10 or if the initial list is odd
     dict_opposites = {"n": "s", "s": "n", "e": "w", "w": "e"}
-    if len(walk) > 10 or not len(walk) % 2:
+    if len(walk) > 10 or len(walk) % 2:
         return False
     while True:
         currentLen = len(walk)
