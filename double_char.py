e56c38696a6db5bb301f167cee3a4761e739c12a
gmahone <gmahone@gmail.com>
2022-06-13 22:02:01 +0200

    add another formulation of string multiplication

--- double_char.py
+++ double_char.py
@@ -10,3 +10,10 @@ def double_char(s):
 # string multiplication solution
 def double_char(s):
     return ''.join(c * 2 for c in s)
+
+# another formulation of string multiplication
+def double_char(s):
+    res = ''
+    for i in s:
+        res += i*2
+    return res
