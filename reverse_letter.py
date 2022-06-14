af9a159f96fb2de43f4e3173978043afc0fb3b26
gmahone <gmahone@gmail.com>
2022-06-14 21:49:17 +0200

    working on initial solution

--- reverse_letter.py
+++ reverse_letter.py
@@ -2,5 +2,5 @@ def alpha_test(char):
     return char.isalpha()
 
 def reverse_letter(string):
-    alpha_only = filter(alpha_test, list(string))
+    alpha_only = list(filter(alpha_test, list(string)))
     return "".join(alpha_only[::-1])
