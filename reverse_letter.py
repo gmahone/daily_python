f337fc03a1a9f8292cbc581ff6af9007f2a7daf4
gmahone <gmahone@gmail.com>
2022-06-14 21:51:03 +0200

    add solution -for in if-

--- reverse_letter.py
+++ reverse_letter.py
@@ -4,3 +4,9 @@ def alpha_test(char):
 def reverse_letter(string):
     alpha_only = list(filter(alpha_test, list(string)))
     return "".join(alpha_only[::-1])
+
+# other solutions
+
+# classic python formulaic method
+def reverse_letter(s):
+  return ''.join([i for i in s if i.isalpha()])[::-1]
