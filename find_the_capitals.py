30b6961e788733974688f9aeacbffefa16f3b82f
gmahone <gmahone@gmail.com>
2022-06-15 22:35:49 +0200

    bugfix initial solution

--- find_the_capitals.py
+++ find_the_capitals.py
@@ -2,6 +2,9 @@ def define_capital(char):
     return 64 < ord(char) < 91
 
 def capitals(word):
-    word_list = filter(define_capital, list(word))
-    return list(word_list)
-    
+    word_list = list(word)
+    result = []
+    for i in range(0, len(word_list)):
+        if define_capital(word_list[i]):
+            result.append(i)
+    return result
