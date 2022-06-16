2a023a816bd7b7b9401b2ad74a596b2e7a36614f
gmahone <gmahone@gmail.com>
2022-06-16 22:23:51 +0200

    add solution using max

--- terminal_game_combat.py
+++ terminal_game_combat.py
@@ -1,3 +1,9 @@
 def combat(health, damage):
     result = health - damage
     return result if result >= 0 else 0 
+
+# other solutions
+
+# solution using max
+def combat(health, damage):
+    return max(0, health - damage)
