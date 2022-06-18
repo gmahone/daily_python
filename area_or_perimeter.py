7e1bf3df7e5af8bd21403d5fd234ae694b9813fb
gmahone <gmahone@gmail.com>
2022-06-18 09:29:30 +0200

    add one line if else formulation

--- area_or_perimeter.py
+++ area_or_perimeter.py
@@ -3,3 +3,7 @@ def area_or_perimeter(l , w):
         return l * w
     else:
         return 2 * (l + w)
+
+# one line if else
+def area_or_perimeter(l, w):
+    return l * w if l == w else (l + w) * 2
