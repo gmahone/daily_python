2c3ae4a31f3f03c9b134507e95e91ec1e58ca9a6
gmahone <gmahone@gmail.com>
2022-06-18 09:30:18 +0200

    add lambda function version

--- area_or_perimeter.py
+++ area_or_perimeter.py
@@ -7,3 +7,7 @@ def area_or_perimeter(l , w):
 # one line if else
 def area_or_perimeter(l, w):
     return l * w if l == w else (l + w) * 2
+
+
+# lambda function version
+area_or_perimeter = lambda a, b : a * b if a == b else 2 * (a + b)
