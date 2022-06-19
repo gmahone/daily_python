4645fa41aeeffaf63c9508331be2047304b81d08
gmahone <gmahone@gmail.com>
2022-06-19 22:24:01 +0200

    add test for fun

--- char_from_ascii.py
+++ char_from_ascii.py
@@ -3,3 +3,13 @@ def get_char(c):
 
 # lambda version
 get_char = lambda c: chr(c)
+
+
+
+
+# add test
+import unittest
+   
+class TestGetChar(unittest.TestCase):
+    def test_get_char_return_A_when_given_65(self):
+        self.assertEqual(get_char(65), 'A')
