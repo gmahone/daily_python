2e376d62f31ce3be73af907eea17ccfdb846f14b
gmahone <gmahone@gmail.com>
2022-06-19 17:52:30 +0200

    working on initial solution

--- help_the_bookseller.py
+++ help_the_bookseller.py
@@ -2,7 +2,7 @@ def stock_list(listOfArt, listOfCat):
     art_dict = {}
     for i in listOfArt:
         split_temp = i.split(" ")
-        if i[0][0] in art_dict:
-            art_dict[i[0][0]] += int(i[1])
+        if split_temp[0][0] in art_dict:
+            art_dict[split_temp[0][0]] += int(split_temp[1])
         else:
-            art_dict[i[0][0]] = int(i[1])
+            art_dict[split_temp[0][0]] = int(split_temp[1])
