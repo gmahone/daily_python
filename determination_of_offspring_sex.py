8c9164d7167df99d5f6429725c75f76b0b690731
gmahone <gmahone@gmail.com>
2022-06-20 21:51:19 +0200

    add solution using format

--- determination_of_offspring_sex.py
+++ determination_of_offspring_sex.py
@@ -1,3 +1,8 @@
 def chromosome_check(sperm):
     offspring_sex = "son" if "Y" in sperm else "daughter"
     return f"Congratulations! You\'re going to have a {offspring_sex}."
+
+
+# using format
+def chromosome_check(sperm):
+    return 'Congratulations! You\'re going to have a {}.'.format('son' if 'Y' in sperm else 'daughter')
