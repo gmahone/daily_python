2c106b245b1511f8fc14eeea8c3a085cfcd19f6a
gmahone <gmahone@gmail.com>
2022-06-20 21:49:24 +0200

    working on initial solution

--- determination_of_offspring_sex.py
+++ determination_of_offspring_sex.py
@@ -1,3 +1,3 @@
 def chromosome_check(sperm):
     offspring_sex = "son" if "X" in sperm else "daughter"
-    return `Congratulations, you\'re going to have a ${offspring_sex}`
+    return f"Congratulations, you\'re going to have a {offspring_sex}"
