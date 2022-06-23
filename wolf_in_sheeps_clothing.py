f7569c1ac76f799f012eb097da84d01c90ae2b1c
gmahone <gmahone@gmail.com>
2022-06-23 21:26:10 +0200

    working on initial solution

--- wolf_in_sheeps_clothing.py
+++ wolf_in_sheeps_clothing.py
@@ -1,2 +1,7 @@
 def warn_the_sheep(queue):
-    pass
+    wolf_index = len(queue) - queue.find("wolf")
+    if wolf_index == 1:
+        result = "Pls go away and stop eating my sheep"
+    else:
+        result = `Oi! Sheep number {wolf_index}! You are about to be eaten by a wolf!`
+    return result
