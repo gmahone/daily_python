2dd03ca9653663735c2ad92fa2d9bfa01b42a930
gmahone <gmahone@gmail.com>
2022-06-23 21:38:44 +0200

    feat: add solution with loop not map

--- sum_of_minimums.py
+++ sum_of_minimums.py
@@ -1,2 +1,7 @@
 def sum_of_minimums(numbers):
    return sum(map(min, numbers))
+
+
+## can work with our without list[] call
+def sum_of_minimums(numbers):
+    return sum([min(rows) for rows in numbers])
