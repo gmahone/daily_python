f99fdfb1a1066a82ce919036115341470884a526
gmahone <gmahone@gmail.com>
2022-06-24 21:28:43 +0200

    feat: add lambda version

--- surface_area_and_volume.py
+++ surface_area_and_volume.py
@@ -2,3 +2,6 @@ def get_size(w,h,d):
     volume = w * h * d
     surface_area = (2 * w * h) + (2 * w * d) + (2 * h * d)
     return [surface_area, volume]
+
+# lambda version
+get_size = lambda w,h,d: [(2 * w * h) + (2 * w * d) + (2 * h * d), w * h * d]
