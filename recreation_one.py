00eb45d1748ba92a3469f42dc2a0489c8ff7fb9d
gmahone <gmahone@gmail.com>
2022-06-29 22:14:29 +0200

    feat: add solution using a global cache

--- recreation_one.py
+++ recreation_one.py
@@ -15,3 +15,25 @@ def list_squared(m, n):
         if int(sqrt_sum) == sqrt_sum:
             result.append([i, sum_squared_list])
     return result
+
+
+## solution using a global cache
+CACHE = {}
+
+def squared_cache(number):
+    if number not in CACHE:
+        divisors = [x for x in range(1, number + 1) if number % x == 0]
+        CACHE[number] = sum([x * x for x in divisors])
+        return CACHE[number] 
+    
+    return CACHE[number]
+
+def list_squared(m, n):
+    ret = []
+
+    for number in range(m, n + 1):
+        divisors_sum = squared_cache(number)
+        if (divisors_sum ** 0.5).is_integer():
+            ret.append([number, divisors_sum])
+
+    return ret
