1dd8b7ee7eaed0f90dce966689867f1fae6553ea
gmahone <gmahone@gmail.com>
2022-06-29 22:20:17 +0200

    feat: add solution that minimizes the divisor search space

--- recreation_one.py
+++ recreation_one.py
@@ -37,3 +37,27 @@ def list_squared(m, n):
             ret.append([number, divisors_sum])
 
     return ret
+
+
+# optimized solution in divisor search space
+# get the divisors: iterate up to sqrt(n), check if the integer divides n with no remainder
+# return the sum of the divisors squared.
+def get_divisors_sum(n):
+    divs=[1]
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            divs.extend([i, int(n/i)])
+    divs.extend([n])
+    
+    # Get sum, return the sum 
+    sm = sum([d**2 for d in list(set(divs))])
+    return sm
+
+# Search for squares amongst the sum of squares of divisors of numbers from m to n
+def list_squared(m, n):
+    out = []
+    for j in range(m,n+1):
+        s = get_divisors_sum(j)
+        if  (s ** 0.5).is_integer():
+            out.append([j, s])
+    return out
