3c1d002e1cce92dcab24784fd844dc92d7701d2b
gmahone <gmahone@gmail.com>
2022-06-29 21:52:36 +0200

    feat: adding smaller version

--- recreation_one.py
+++ recreation_one.py
@@ -23,3 +23,22 @@ def list_squared(m, n):
         if int(sqrt_sum) == sqrt_sum:
             result.append([i, sum_squared_list])
     return result
+
+
+## smaller version
+def list_squared(m, n):
+    result = []
+    for i in range(m, n+1):
+        divisors = [1]
+        if not i == 1:
+            for j in range(2, int(i/2)+1):
+                if i % j == 0:
+                    divisors.append(j)
+            divisors.append(i)
+        sum_squared_list = sum(number**2 for number in divisors)
+        sqrt_sum = sum_squared_list**0.5
+        if len(divisors) == 2 and not i == 1:
+            continue        
+        if int(sqrt_sum) == sqrt_sum:
+            result.append([i, sum_squared_list])
+    return result
