9ba508cd16f9fcd5d38e3215b4276d05625e0516
gmahone <gmahone@gmail.com>
2022-06-29 22:13:57 +0200

    feat: add barely passing solution

--- recreation_one.py
+++ recreation_one.py
@@ -1,31 +1,4 @@
-def find_divisors(x):
-    if x == 1:
-        return [1]
-    else:
-        result = [1];
-        for i in range(2, int(x/2)+1):
-            if x % i == 0:
-                result.append(i)
-        result.append(x)
-        return result
-
-def square_num(x):
-    return x*x
-
-def list_squared(m, n):
-    result = []
-    for i in range(m, n+1):
-        divisors = find_divisors(i)
-        sum_squared_list = sum(map(square_num, divisors))
-        sqrt_sum = sum_squared_list**0.5
-        if len(divisors) == 2 and not i == 1:
-            continue        
-        if int(sqrt_sum) == sqrt_sum:
-            result.append([i, sum_squared_list])
-    return result
-
-
-## smaller version
+# passed solution
 def list_squared(m, n):
     result = []
     for i in range(m, n+1):
