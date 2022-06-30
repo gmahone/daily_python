f6b2e0544494a1aafc8f001a1addf72875c430cb
gmahone <gmahone@gmail.com>
2022-06-30 16:50:39 +0200

    feat: add solution using sum of sums

--- how_many_stairs.py
+++ how_many_stairs.py
@@ -1,3 +1,7 @@
 def stairs_in_20(stairs):
     raw_counts = [day for days in stairs for day in days]
     return sum(raw_counts) * 20
+
+# solution using sum of sums
+def stairs_in_20(stairs):
+    return sum(sum(day) for day in stairs) * 20
