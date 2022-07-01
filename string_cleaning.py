8ff412b00c21227ed4898c311a8ff8689cd7ef63
gmahone <gmahone@gmail.com>
2022-07-01 22:19:59 +0200

    feat: add solution using replace

--- string_cleaning.py
+++ string_cleaning.py
@@ -8,3 +8,10 @@ def string_clean(s):
 # using isdigit()
 def string_clean(s):
     return ''.join(x for x in s if not x.isdigit())
+
+# using replace
+def string_clean(s):
+    removeString = "0123456789"
+    for d in removeString:
+        s = s.replace(d, "")
+    return s
