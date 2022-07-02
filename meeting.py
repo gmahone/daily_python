73b635dfe4d6b33bc6ea543e57c31df937cab529
gmahone <gmahone@gmail.com>
2022-07-02 17:23:19 +0200

    fix: formatting of solution

--- meeting.py
+++ meeting.py
@@ -1,5 +1,5 @@
 def meeting(s):
     s_split = s.split(";")
-    s_split = [i.split(':')[::-1] for i in s_split]
-    s_split = list(map(lambda x: x[0] + "," + x[1], s_split))
-    return sorted(s_split)
+    s_split = [i.upper().split(':')[::-1] for i in s_split]
+    s_split = list(map(lambda x: "(" + x[0] + ", " + x[1] + ")", s_split))
+    return "".join(sorted(s_split))
