30c3ff9c47be149838adb447ea9dcd42b22f1e9d
gmahone <gmahone@gmail.com>
2022-07-02 17:27:10 +0200

    feat: add concise solution

--- meeting.py
+++ meeting.py
@@ -3,3 +3,8 @@ def meeting(s):
     s_split = [i.upper().split(':')[::-1] for i in s_split]
     s_split = list(map(lambda x: "(" + x[0] + ", " + x[1] + ")", s_split))
     return "".join(sorted(s_split))
+
+
+# beatiful one-liner
+def meeting(s):
+    return ''.join(sorted('({1}, {0})'.format(*(x.split(':'))) for x in s.upper().split(';')))
