555d738e2e09d290bc2fc5ffb0da4c5909d675d9
gmahone <gmahone@gmail.com>
2022-07-02 17:18:33 +0200

    feat: working on initial solution

--- meeting.py
+++ meeting.py
@@ -2,4 +2,4 @@ def meeting(s):
     s_split = s.split(";")
     s_split = [i.split(':')[::-1] for i in s_split]
     s_split = list(map(lambda x: x[0] + "," + x[1], s_split))
-    print(sorted(s_split))
+    return sorted(s_split)
