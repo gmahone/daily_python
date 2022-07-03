f6baf76d2410d410a8a715d84128f5bf71171f53
gmahone <gmahone@gmail.com>
2022-07-03 21:40:23 +0200

    feat: add lambda function versions

--- fundamentals_return.py
+++ fundamentals_return.py
@@ -15,3 +15,11 @@ def exponent(a,b):
 
 def subt(a,b):
     return a - b
+
+# as lambda functions
+add = lambda a, b: a + b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+mod = lambda a, b: a % b
+exponent = lambda a, b: a ** b
+subt = lambda a, b:a - b
