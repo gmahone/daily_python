74a0f4d27b46b9659a98ff1b8954f4f847f494e0
gmahone <gmahone@gmail.com>
2022-07-03 21:48:24 +0200

    feat: add initial solution

--- drink_about.py
+++ drink_about.py
@@ -1,2 +1,10 @@
 def people_with_age_drink(age):
-    pass
+    if age < 14:
+        drink = "toddy"
+    elif age < 18:
+        drink = "coke"
+    elif age < 21:
+        drink = "beer"
+    else:
+        drink = "whisky"
+    return f"drink {drink}"
