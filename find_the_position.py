497f18f29cb82c6711d7a2ff156029140ce838d8
gmahone <gmahone@gmail.com>
2022-07-04 16:45:50 +0200

    feat: add solution using string formatting and index finding

--- find_the_position.py
+++ find_the_position.py
@@ -4,3 +4,7 @@ def position(alphabet):
 # same solution with format
 def position(alphabet):
     return "Position of alphabet: {}".format(ord(alphabet) - 96)
+
+# another format using index finding and sprintf like
+def position(alphabet):
+    return "Position of alphabet: %s" % ("abcdefghijklmnopqrstuvwxyz".find(alphabet) + 1)
