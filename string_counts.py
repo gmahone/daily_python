bb3ceb690f9e377e7e74e4c0fb9b83cb5e23c28d
gmahone <gmahone@gmail.com>
2022-07-04 16:58:40 +0200

    feat: add loop solution

--- string_counts.py
+++ string_counts.py
@@ -4,3 +4,12 @@ def str_count(strng, letter):
 # don't need to create a list first
 def strCount(string, letter):
     return string.count(letter)
+
+
+# looping solution for laughs
+def str_count(strng, letter):
+    result = 0
+    for chr in strng:
+        if chr == letter:
+            result += 1
+    return result
