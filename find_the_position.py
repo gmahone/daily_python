471716d272b6996d79591c81ff7dedfbf30e0689
gmahone <gmahone@gmail.com>
2022-07-04 16:42:07 +0200

    feat: add solution with .format

--- find_the_position.py
+++ find_the_position.py
@@ -1,2 +1,6 @@
 def position(alphabet):
     return f"Position of alphabet: {ord(alphabet) - 96}"
+
+# same solution with format
+def position(alphabet):
+    return "Position of alphabet: {}".format(ord(alphabet) - 96)
