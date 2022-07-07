b4bb25b7837dedba339ff838b4955fd8322296f8
Greg Mahone <gmahone@gmail.com>
2022-07-07 21:50:10 +0200

    feat: add labeled solution using re

--- regex_password_validation.py
+++ regex_password_validation.py
@@ -1,1 +1,14 @@
 regex="^(?=.{6,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(^[A-Za-z0-9]+$)"
+
+# clearly labeled solution using re
+from re import compile, VERBOSE
+
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
