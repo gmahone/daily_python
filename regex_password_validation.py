af3de9dec92ad6f572ebb99186fa41c712ded3cc
Greg Mahone <gmahone@gmail.com>
2022-07-07 21:50:49 +0200

    feat: add labeled solution not using re

--- regex_password_validation.py
+++ regex_password_validation.py
@@ -12,3 +12,14 @@ regex = compile("""
 {6,}           # at least 6 characters long
 $              # end word
 """, VERBOSE)
+
+# labeled solution not using re
+regex = (
+    '^'            # start line
+    '(?=.*\d)'     # must contain one digit from 0-9
+    '(?=.*[a-z])'  # must contain one lowercase characters
+    '(?=.*[A-Z])'  # must contain one uppercase characters
+    '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
+    '{6,}'         # length at least 6 chars
+    '$'            # end line
+)
