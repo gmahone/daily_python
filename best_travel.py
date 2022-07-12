41bd3a69d61e48223a1527e231831aa148a343f6
Greg Mahone <gmahone@gmail.com>
2022-07-12 22:12:32 +0200

    add text description

--- best_travel.py
+++ best_travel.py
@@ -1,2 +1,6 @@
+# find k unique values of a list ls
+#  that sum up to a value as closely as possible
+#  to a given value t (inclusive), but do not go over it
+
 def choose_best_sum(t, k, ls):
     pass
