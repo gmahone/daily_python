688d288ad38c4eaa9e03811388f41aa0be369db9
Greg Mahone <gmahone@gmail.com>
2022-07-12 22:26:16 +0200

    working on pseudocode

--- best_travel.py
+++ best_travel.py
@@ -2,5 +2,7 @@
 #  that sum up to a value as closely as possible
 #  to a given value t (inclusive), but do not go over it
 
+# need to produce all possible combinations of k values
+
 def choose_best_sum(t, k, ls):
     pass
