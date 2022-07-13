005274226f0ce1bb055b315c454cf867c04e7a9f
Greg Mahone <gmahone@gmail.com>
2022-07-13 21:13:21 +0200

    feat: add example code

--- best_travel.py
+++ best_travel.py
@@ -4,5 +4,14 @@
 
 # need to produce all possible combinations of k values
 
+# example code for itertools
+import itertools
+
+stuff = [1, 2, 3]
+for L in range(0, len(stuff)+1):
+    for subset in itertools.combinations(stuff, L):
+        print(subset)
+## end example
+
 def choose_best_sum(t, k, ls):
     pass
