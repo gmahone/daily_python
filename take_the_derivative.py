3321f9e47781a79e56eb80c0b68dbc7d7a7b1d6b
Greg Mahone <gmahone@gmail.com>
2022-07-13 22:00:40 +0200

    feat: add solution using format

--- take_the_derivative.py
+++ take_the_derivative.py
@@ -1,2 +1,6 @@
 def derive(coefficient, exponent): 
     return f"{coefficient*exponent}x^{exponent-1}" if exponent > 2 else f"{coefficient*exponent}x"
+
+# solution using format
+def derive(coefficient, exponent): 
+    return("{}x^{}".format(coefficient*exponent, exponent-1))
