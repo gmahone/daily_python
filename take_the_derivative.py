b8e2d7eebb6dc372e08a422d159f7dabd2ebd3fa
Greg Mahone <gmahone@gmail.com>
2022-07-13 21:59:12 +0200

    fix: return string

--- take_the_derivative.py
+++ take_the_derivative.py
@@ -1,2 +1,2 @@
 def derive(coefficient, exponent): 
-    return f"{coefficient*exponent}^{exponent-1}" if exponent > 2 else f"{coefficient*exponent}"
+    return f"{coefficient*exponent}x^{exponent-1}" if exponent > 2 else f"{coefficient*exponent}x"
