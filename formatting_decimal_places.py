8e57fa1d8c35f95aae252d8dfe8bffc53e637a42
Greg Mahone <gmahone@gmail.com>
2022-07-15 22:47:30 +0200

    feat: add solution using format

--- formatting_decimal_places.py
+++ formatting_decimal_places.py
@@ -4,3 +4,8 @@ def two_decimal_places(n):
 ## another way with full round
 def two_decimal_places(n):
    return round(n * 100) / 100
+
+# another solution with format
+def two_decimal_places(n):
+    return float("{0:.2f}".format(n))
+
