cad744a371666a8b50ad43a94155f9257671ab7d
Greg Mahone <gmahone@gmail.com>
2022-07-16 21:46:35 +0200

    fix: add more trial solutions

--- regex_basics.py
+++ regex_basics.py
@@ -7,3 +7,13 @@ def is_digit(n):
 def is_digit(n):
     print(n)
     return n.isdigit() & bool(len(n.strip()))
+
+import re
+def is_digit(n):
+    return bool(re.search("[0-9]"))
+                
+                
+import re
+def is_digit(n):
+    print(n);
+    return bool(re.search(n, "^[0123456789]$")) & bool(len(n.strip()))
