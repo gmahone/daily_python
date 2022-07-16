02142d2ff7073725aae38d3cba85b32423e215e4
Greg Mahone <gmahone@gmail.com>
2022-07-16 22:04:51 +0200

    comment out non-working solutions

--- regex_basics.py
+++ regex_basics.py
@@ -1,27 +1,24 @@
-import re
+#import re
+#def is_digit(n):
+#    return len(n) & n.isdigit() & bool(re.search(n, "0123456789"))
 
-def is_digit(n):
-    return len(n) & n.isdigit() & bool(re.search(n, "0123456789"))
+#def is_digit(n):
+#    print(n)
+#    return n.isdigit() & bool(len(n.strip()))
 
+#import re
+#def is_digit(n):
+#    return bool(re.search("[0-9]"))
+               
+#import re
+#def is_digit(n):
+#    print(n);
+#    return bool(re.search(n, "^[0123456789]$")) & bool(len(n.strip()))
 
-def is_digit(n):
-    print(n)
-    return n.isdigit() & bool(len(n.strip()))
-
-import re
-def is_digit(n):
-    return bool(re.search("[0-9]"))
-                
-                
-import re
-def is_digit(n):
-    print(n);
-    return bool(re.search(n, "^[0123456789]$")) & bool(len(n.strip()))
-
-import re
-def is_digit(n):
-    print(n);
-    return bool(re.search(n, "^[\W_][0123456789]*$")) & bool(len(n.strip()))
+#import re
+#def is_digit(n):
+#    print(n);
+#    return bool(re.search(n, "^[\W_][0123456789]*$")) & bool(len(n.strip()))
 
 
 def is_digit(n):
