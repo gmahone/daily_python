b413b5f3cf750eb9368de62908141946a49f3d73
Greg Mahone <gmahone@gmail.com>
2022-07-17 21:00:19 +0200

    feat: add cleaner solution

--- remove_first_and_last_part2.py
+++ remove_first_and_last_part2.py
@@ -1,3 +1,7 @@
 def array(string):
     result = string.split(",")
     return None if len(result) < 3 else " ".join(result[1:len(result)-1])
+
+# solution with less separation
+def array(string):
+  return " ".join(string.split(",")[1:-1]) or None
