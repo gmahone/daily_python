bcbbe8952b51b033bb25c9c608d1bfc26f034ae3
Greg Mahone <gmahone@gmail.com>
2022-07-17 20:59:09 +0200

    fix: replace slice with normal indexing

--- remove_first_and_last_part2.py
+++ remove_first_and_last_part2.py
@@ -1,3 +1,3 @@
 def array(string):
     result = string.split(",")
-    return None if len(result) < 3 else " ".join(result.slice(1,len(result)))
+    return None if len(result) < 3 else " ".join(result[1:len(result)-1])
