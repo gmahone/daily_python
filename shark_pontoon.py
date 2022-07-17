d4808dce40c8aa919056d50acad222ac03fb9fc2
Greg Mahone <gmahone@gmail.com>
2022-07-17 22:09:46 +0200

    feat: add initial solution

--- shark_pontoon.py
+++ shark_pontoon.py
@@ -1,4 +1,6 @@
 def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
     you_time = pontoon_distance/you_speed
     shark_time = shark_distance/shark_speed
-    
+    if dolphin:
+        shark_time *= 2
+    return "Alive!" if you_time < shark_time else "Shark Bait!"
