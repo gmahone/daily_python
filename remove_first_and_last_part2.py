a2ec258a16a7e06bc681924559f5b7111022763e
Greg Mahone <gmahone@gmail.com>
2022-07-17 21:00:45 +0200

    fix: original solution incorporating more python index method

--- remove_first_and_last_part2.py
+++ remove_first_and_last_part2.py
@@ -1,6 +1,6 @@
 def array(string):
     result = string.split(",")
-    return None if len(result) < 3 else " ".join(result[1:len(result)-1])
+    return None if len(result) < 3 else " ".join(result[1:-1])
 
 # solution with less separation
 def array(string):
