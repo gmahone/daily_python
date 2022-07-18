f8299e47e239cc7c60cf13b1f45ff482d0f71f12
Greg Mahone <gmahone@gmail.com>
2022-07-18 20:57:13 +0200

    feat: add math solution

--- power_of_two.py
+++ power_of_two.py
@@ -3,3 +3,8 @@ def power_of_two(x):
   while 2**curr_power < x:
     curr_power += 1
   return True if 2**curr_power == x else False
+
+
+# math solution
+def power_of_two(x):
+    return x != 0 and ((x & (x - 1)) == 0)
