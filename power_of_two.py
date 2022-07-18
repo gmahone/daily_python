df286e6dd762da2b1c5e11c5b0fdc759cb8ff5b2
Greg Mahone <gmahone@gmail.com>
2022-07-18 20:54:51 +0200

    fix: update variable name

--- power_of_two.py
+++ power_of_two.py
@@ -1,5 +1,5 @@
 def power_of_two(x):
-  power = 0
-  while 2**power < x:
-    power += 1
-  return True if 2**power == x else False
+  curr_power = 0
+  while 2**curr_power < x:
+    curr_power += 1
+  return True if 2**curr_power == x else False
