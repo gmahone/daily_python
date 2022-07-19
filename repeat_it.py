a97dae330ef77df134b30164a4ba1222989b1238
Greg Mahone <gmahone@gmail.com>
2022-07-19 21:10:47 +0200

    fix: early break if wrong type

--- repeat_it.py
+++ repeat_it.py
@@ -1,5 +1,7 @@
 def repeat_it(string,n):
+    if not isinstance(string, str):
+        return "Not a string"
     result = string
     for i in range(1,n):
         result += string
-    return result if isinstance(string, str) else "Not a string"
+    return result
