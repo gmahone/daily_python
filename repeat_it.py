4b2c9d9520c86f0535bbd313594d7e3a32c41675
Greg Mahone <gmahone@gmail.com>
2022-07-19 21:13:13 +0200

    feat: add solution with string multiplication

--- repeat_it.py
+++ repeat_it.py
@@ -5,3 +5,7 @@ def repeat_it(string,n):
     for i in range(0,n):
         result += string
     return result
+
+# * works on strings
+def repeat_it(string,n):
+    return string * n if isinstance(string,str) else 'Not a string'
