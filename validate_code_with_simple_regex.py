2f727ecf9cc0b15cbb179b2b9eb0ba96c78e4b6b
Greg Mahone <gmahone@gmail.com>
2022-07-20 21:54:54 +0200

    feat: add non-regex solution using startswith

--- validate_code_with_simple_regex.py
+++ validate_code_with_simple_regex.py
@@ -2,3 +2,8 @@ import re
 
 def validate_code(code):
     return bool(re.findall("^[123]", str(code)))
+
+
+# non regex solution
+def validate_code(code):
+    return str(code).startswith(('1', '2', '3'))
