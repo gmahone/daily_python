98dd9472375c7bd1fa2404ec04b1eded023a88de
Greg Mahone <gmahone@gmail.com>
2022-07-21 22:32:16 +0200

    feat: add solution using loop

--- kata_example_twist.py
+++ kata_example_twist.py
@@ -2,3 +2,8 @@ websites = ["codewars"]*1000
 
 # add solution using range
 websites = ['codewars' for _ in range(1000)]
+
+# add solution using loop
+websites = []
+for index in range(0,1000):
+    websites.append("codewars")
