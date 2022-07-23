6e5dcfefffa16e64fca3a82a7782878dde22f652
Greg Mahone <gmahone@gmail.com>
2022-07-23 21:54:34 +0200

    feat: add alternative format

--- pixelart_planning.py
+++ pixelart_planning.py
@@ -1,2 +1,7 @@
 def is_divisible(wall_length, pixel_size):
     return wall_length % pixel_size == 0
+
+
+# alternative solution
+def is_divisible(wall_length, pixel_size):
+    return not wall_length % pixel_size
