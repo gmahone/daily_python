22f31ad837c8011e7be8c53b581bf27c20b14481
Greg Mahone <gmahone@gmail.com>
2022-07-24 21:58:24 +0200

    feat: add lambda version

--- the_if_function.py
+++ the_if_function.py
@@ -4,3 +4,6 @@ def _if(bool, func1, func2):
 # alternative calling
 def _if(bool, func1, func2):
   (bool and func1 or func2)()
+
+# lambda function version
+_if = lambda bool, func1, func2: func1() if bool else func2()
