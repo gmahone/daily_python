ffbe3b3c11481be26a833b3597910904a3167235
Greg Mahone <gmahone@gmail.com>
2022-07-26 08:58:42 +0200

    feat: add solution using map to str elements

--- print_array_comma.py
+++ print_array_comma.py
@@ -2,3 +2,8 @@ def print_array(arr):
     for i in range(0, len(arr)):
         arr[i] = str(arr[i])
     return ",".join(arr)
+
+
+# using map to str elements
+def print_array(arr):
+    return ','.join(map(str, arr))
