ec15d1a20f69ec62c002058ac8ebe7880b7277bc
Greg Mahone <gmahone@gmail.com>
2022-08-12 21:53:58 +0200

    feat: add alternative solution layout

--- is_it_a_number.py
+++ is_it_a_number.py
@@ -4,3 +4,11 @@ def isDigit(string):
     except:
         return False
     return True
+
+# alternative layout
+def isDigit(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False
