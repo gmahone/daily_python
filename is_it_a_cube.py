6211c9fd5c1275b6c8f1e15816acf138bcbac5a3
Greg Mahone <gmahone@gmail.com>
2022-08-12 21:58:53 +0200

    feat: add simpler solution formulation

--- is_it_a_cube.py
+++ is_it_a_cube.py
@@ -1,2 +1,7 @@
 def cube_checker(volume, side):
     return True if side**3 == abs(volume) and volume != 0 and volume > 0 else False
+
+
+## simpler formulation
+def cube_checker(volume, side):
+    return 0 < volume == side**3
