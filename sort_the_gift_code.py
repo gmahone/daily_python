eb0a7e40b422ae34a3ba8d0c43163d97e51e9aed
Greg Mahone <gmahone@gmail.com>
2022-08-16 20:45:41 +0200

    feat: add lambda solution

--- sort_the_gift_code.py
+++ sort_the_gift_code.py
@@ -5,3 +5,6 @@ def sort_gift_code(code):
 # shorter solution without making list
 def sort_gift_code(code):
     return "".join(sorted(code))
+
+# lambda version
+sort_gift_code = lambda code: ''.join(sorted(code))
