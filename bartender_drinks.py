f759bcef4bbbfe19739a9d7f7b35283741201338
Greg Mahone <gmahone@gmail.com>
2022-08-17 15:25:02 +0200

    feat: add string search if else

--- bartender_drinks.py
+++ bartender_drinks.py
@@ -36,3 +36,12 @@ def get_drink_by_profession(param):
         "Politician": "Your tax dollars",
         "Rapper": "Cristal"
     }.get(param.title(), "Beer")
+
+
+## if else for string search
+def get_drink_by_profession(param):
+    d = {"Jabroni":"Patron Tequila", "School Counselor":"Anything with Alcohol", "Programmer":"Hipster Craft Beer", "Bike Gang Member":"Moonshine", "Politician":"Your tax dollars", "Rapper":"Cristal"}
+    if param.title() in d:
+        return d[param.title()]
+    else:
+        return "Beer"
