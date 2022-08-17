8baa79633ba6e43f00b990c2eefa4b60a0fce491
Greg Mahone <gmahone@gmail.com>
2022-08-17 15:24:23 +0200

    feat: add solution with string get on return

--- bartender_drinks.py
+++ bartender_drinks.py
@@ -24,3 +24,15 @@ d = {
 
 def get_drink_by_profession(s):
     return d.get(s.lower(), "Beer")
+
+
+## string search in return using title case
+def get_drink_by_profession(param):
+    return {
+        "Jabroni": "Patron Tequila",
+        "School Counselor": "Anything with Alcohol",
+        "Programmer": "Hipster Craft Beer",
+        "Bike Gang Member": "Moonshine",
+        "Politician": "Your tax dollars",
+        "Rapper": "Cristal"
+    }.get(param.title(), "Beer")
