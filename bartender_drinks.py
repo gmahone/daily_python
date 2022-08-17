88c6dd87df8ce9f5cc876645207f74ba1c9bcfc0
Greg Mahone <gmahone@gmail.com>
2022-08-17 15:20:04 +0200

    feat: add initial solution

--- bartender_drinks.py
+++ bartender_drinks.py
@@ -1,2 +1,9 @@
 def get_drink_by_profession(param):
-    pass
+    result_dict = {"Jabroni": "Patron Tequila",
+                  "School Counselor": "Anything with Alcohol",
+                  "Programmer": "Hipster Craft Beer",
+                  "Bike Gang Member": "Moonshine",
+                  "Politician": "Your tax dollars",
+                  "Rapper": "Cristal"}
+    result = result_dict[param] if result_dict[param] else "Beer"
+    return result
