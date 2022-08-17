bbd767fa7ce0643b1f6073396c6508bcf9c9e14a
Greg Mahone <gmahone@gmail.com>
2022-08-17 15:25:48 +0200

    feat: add solution with all cases via elif

--- bartender_drinks.py
+++ bartender_drinks.py
@@ -45,3 +45,22 @@ def get_drink_by_profession(param):
         return d[param.title()]
     else:
         return "Beer"
+
+    
+## longform via elif
+def get_drink_by_profession(param):
+    newparam = param.lower()
+    if newparam == 'jabroni':
+        return 'Patron Tequila'
+    elif newparam == 'school counselor':
+        return 'Anything with Alcohol'
+    elif newparam == 'programmer':
+        return 'Hipster Craft Beer'
+    elif newparam == 'bike gang member':
+        return "Moonshine"
+    elif newparam == 'politician':
+        return "Your tax dollars"
+    elif newparam == "rapper":
+        return "Cristal"
+    else:
+        return "Beer"
