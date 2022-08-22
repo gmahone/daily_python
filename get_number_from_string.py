d53d8c4d8582accd7c6e4b36946819e3a7e4a2c0
Greg Mahone <gmahone@gmail.com>
2022-08-22 20:54:45 +0200

    feat: add solution using isdigit

--- get_number_from_string.py
+++ get_number_from_string.py
@@ -1,2 +1,6 @@
 def get_number_from_string(string):
     return int(''.join(c for c in string if c in '0123456789'))
+
+# using isdigit
+def get_number_from_string(string):
+    return int(''.join(x for x in string if x.isdigit()))
