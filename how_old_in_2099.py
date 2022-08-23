5b1517aa4cb6f4f6b1c21764e72d5978826fc542
Greg Mahone <gmahone@gmail.com>
2022-08-23 21:40:19 +0200

    fix: add plural suffix case

--- how_old_in_2099.py
+++ how_old_in_2099.py
@@ -1,9 +1,13 @@
 def calculate_age(year_of_birth, current_year):
     year_diff = current_year - year_of_birth
     if year_diff > 0:
-        result = "You are {} years old."
+        result = "You are {} year{} old."
     elif year_diff < 0:
-        result = "You will be born in {} years."
+        result = "You will be born in {} year{}."
     else:
         result = "You were born this very year!"
-    return result.format(abs(year_diff))
+    
+    plural_suffix = "s"
+    if abs(year_diff) == 1:
+        plural_suffix = ""
+    return result.format(abs(year_diff), plural_suffix)
