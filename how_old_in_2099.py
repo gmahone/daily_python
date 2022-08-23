2b578adbc8e2671363ba9c08f6ba98a3515ff33c
Greg Mahone <gmahone@gmail.com>
2022-08-23 21:35:21 +0200

    fix: edge case of year = 0

--- how_old_in_2099.py
+++ how_old_in_2099.py
@@ -4,4 +4,6 @@ def calculate_age(year_of_birth, current_year):
         result = "You are {} years old"
     elif year_diff < 0:
         result = "You will be born in {} years"
+    else:
+        result = "You were born this very year!"
     return result.format(abs(year_diff))
