518f7e608c00910f828036fbef097a5be6cca232
Greg Mahone <gmahone@gmail.com>
2022-08-23 21:31:17 +0200

    feat: add initial solution

--- how_old_in_2099.py
+++ how_old_in_2099.py
@@ -1,2 +1,7 @@
 def calculate_age(year_of_birth, current_year):
-    pass
+    year_diff = current_year - year_of_birth
+    if year_diff > 0:
+        result = "You are {} years old"
+    elif year_diff < 0:
+        result = "You will be born in {} years"
+    return result.format(year_diff)
