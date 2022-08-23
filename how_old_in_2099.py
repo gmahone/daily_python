0c80fb2304a0860dbda456767c287fe0ae0b46ca
Greg Mahone <gmahone@gmail.com>
2022-08-23 21:41:04 +0200

    feat: add slimmer solution

--- how_old_in_2099.py
+++ how_old_in_2099.py
@@ -11,3 +11,14 @@ def calculate_age(year_of_birth, current_year):
     if abs(year_diff) == 1:
         plural_suffix = ""
     return result.format(abs(year_diff), plural_suffix)
+
+
+# slimmer solution
+def calculate_age(year_of_birth, current_year):
+    diff = abs(current_year - year_of_birth)
+    plural = '' if diff == 1 else 's'
+    if year_of_birth < current_year:
+        return 'You are {} year{} old.'.format(diff, plural)
+    elif year_of_birth > current_year:
+        return 'You will be born in {} year{}.'.format(diff, plural)
+    return 'You were born this very year!'
