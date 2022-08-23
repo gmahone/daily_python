1f5d61682872e20b5baa9ded3ccefe82132728ee
Greg Mahone <gmahone@gmail.com>
2022-08-23 21:36:06 +0200

    fix: formatting of result

--- how_old_in_2099.py
+++ how_old_in_2099.py
@@ -1,9 +1,9 @@
 def calculate_age(year_of_birth, current_year):
     year_diff = current_year - year_of_birth
     if year_diff > 0:
-        result = "You are {} years old"
+        result = "You are {} years old."
     elif year_diff < 0:
-        result = "You will be born in {} years"
+        result = "You will be born in {} years."
     else:
         result = "You were born this very year!"
     return result.format(abs(year_diff))
