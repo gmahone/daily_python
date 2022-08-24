10e7ecbaacae65f84f475293096138030d8ca000
Greg Mahone <gmahone@gmail.com>
2022-08-24 13:17:10 +0200

    fix: add pad_numbers list

--- build_tower.py
+++ build_tower.py
@@ -1,5 +1,6 @@
 def tower_builder(n_floors):
-    floor_list = range(1,n_floors+1)
-    asterisk_numbers = range(1, (n_floors*2), 2)
-    print(list(asterisk_numbers))
+    floor_list = list(range(1,n_floors+1))
+    asterisk_numbers = list(range(1, (n_floors*2), 2))
+    pad_numbers = list(range(0,n_floors))
+    print(pad_numbers)
     pass
