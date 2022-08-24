e45c2aa22d4f92da29b295991130b3ba5381f937
Greg Mahone <gmahone@gmail.com>
2022-08-24 13:19:49 +0200

    fix: working on return result

--- build_tower.py
+++ build_tower.py
@@ -2,5 +2,8 @@ def tower_builder(n_floors):
     floor_list = list(range(1,n_floors+1))
     asterisk_numbers = list(range(1, (n_floors*2), 2))
     pad_numbers = list(range(0,n_floors))
+    result = []
+    for i in range(0,n_floors):
+        result.append("{}{}{}".format(" "*pad_numbers[i], "*"*asterisk_numbers[+], " "*pad_numbers[i])
     print(pad_numbers)
-    pass
+    return result
