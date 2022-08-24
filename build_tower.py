2795815437f353e2ab7813671648583b5a23d737
Greg Mahone <gmahone@gmail.com>
2022-08-24 13:25:21 +0200

    fix: remove debugging prints

--- build_tower.py
+++ build_tower.py
@@ -2,12 +2,10 @@ def tower_builder(n_floors):
     floor_list = list(range(1,n_floors+1))
     asterisk_numbers = list(range(1, (n_floors*2), 2))
     pad_numbers = list(range(0,n_floors))[::-1]
-    print(pad_numbers)
     result = []
     for i in range(0,n_floors):
         left_pad = " " * pad_numbers[i]
         asterisk_pad = "*" * asterisk_numbers[i]
         right_pad = " " * pad_numbers[i]
         result.append("{}{}{}".format(left_pad, asterisk_pad, right_pad))
-        print(pad_numbers[i])
     return result
