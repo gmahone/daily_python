34c11b2e8779ff86df95204a6d161884cd813ded
Greg Mahone <gmahone@gmail.com>
2022-08-25 21:40:03 +0200

    feat: add solution with predefined statement object

--- for_ufc_fans.py
+++ for_ufc_fans.py
@@ -5,3 +5,13 @@ def quote(fighter):
         return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
     else:
         return "Please either George Saint Pierre or Conor McGregor"
+
+
+# add solution with pre-defined statement
+statements = {
+    'george saint pierre': "I am not impressed by your performance.",
+    'conor mcgregor': "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+}
+
+def quote(fighter):
+    return statements[fighter.lower()]
