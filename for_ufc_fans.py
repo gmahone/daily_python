cb0a7d64bfbaf28236080b22be3099deaca89063
Greg Mahone <gmahone@gmail.com>
2022-08-25 21:41:44 +0200

    fix: add solution

--- for_ufc_fans.py
+++ for_ufc_fans.py
@@ -18,3 +18,6 @@ def quote(fighter):
 
 
 ## solution using get
+def quote(fighter):
+    return {'george saint pierre': 'I am not impressed by your performance.',
+        'conor mcgregor': "I'd like to take this chance to apologize.. To absolutely NOBODY!"}.get(fighter.lower())
