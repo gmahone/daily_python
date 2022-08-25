bca24e59cf0e498e965b47b91cd3da4df9140562
Greg Mahone <gmahone@gmail.com>
2022-08-25 21:37:56 +0200

    feat: add initial solution

--- for_ufc_fans.py
+++ for_ufc_fans.py
@@ -1,2 +1,7 @@
 def quote(fighter):
-    pass
+    if fighter.lower() == "george saint pierre":
+        return "I am not impressed by your performance."
+    elif fighter.lower() == "conor mcgregor":
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+    else:
+        return "Wrong input"
