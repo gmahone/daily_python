2ea2cfba2ee324a61fff1c120f6dc7d297e6e514
Greg Mahone <gmahone@gmail.com>
2022-08-26 22:12:42 +0200

    feat: add solution with alternative indexing

--- largest_pair_sum.py
+++ largest_pair_sum.py
@@ -1,3 +1,8 @@
 def largest_pair_sum(numbers): 
     sorted_list = sorted(numbers)[::-1]
     return sum(sorted_list[0:2])
+
+
+## different indexing
+def largest_pair_sum(numbers): 
+    return sum(sorted(numbers)[-2:])
