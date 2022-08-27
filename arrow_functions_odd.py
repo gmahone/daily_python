e7860c196cd3593e283db1c87820314e24ffd0c3
Greg Mahone <gmahone@gmail.com>
2022-08-27 21:53:07 +0200

    feat: add typical python syntax solution - for in if

--- arrow_functions_odd.py
+++ arrow_functions_odd.py
@@ -2,3 +2,8 @@ def filter_odd(x):
   return x % 2 != 0
 
 odds = lambda a: list(filter(filter_odd, a))
+
+
+## add solution using for in if
+def odds(values):
+        return [i for i in values if i%2]
