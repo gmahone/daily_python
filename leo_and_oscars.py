b365eab5d2c21b2436d7db154088480537cfb021
Greg Mahone <gmahone@gmail.com>
2022-08-28 20:25:56 +0200

    feat: add solution using a map

--- leo_and_oscars.py
+++ leo_and_oscars.py
@@ -7,3 +7,11 @@ def leo(oscar):
     if oscar > 88:
         result = "Leo got one already!"
     return result
+
+# solution with map
+def leo(oscar):
+    if oscar <= 88:
+        return {86: 'Not even for Wolf of wallstreet?!',
+                88: 'Leo finally won the oscar! Leo is happy'}.get(
+            oscar, 'When will you give Leo an Oscar?')
+    return 'Leo got one already!'
