6fe0760d1e14212e6385627730ec6d0fc4050628
Greg Mahone <gmahone@gmail.com>
2022-09-08 20:43:47 +0200

    fix: traverse backwards rather than forwards

--- esrever.py
+++ esrever.py
@@ -1,7 +1,6 @@
 def reverse(lst):
     result = list()
     while len(lst) > 0:
-        print(lst[i])
-        result.append(lst.pop(i))
+        result.append(lst.pop(len(lst)-1))
         print(result)
     return result
