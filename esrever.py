717ff842ec01c8631c428643f6dbd2cdb641220c
Greg Mahone <gmahone@gmail.com>
2022-09-08 20:45:51 +0200

    feat: add solution with non-specific pop index and easier while

--- esrever.py
+++ esrever.py
@@ -3,3 +3,10 @@ def reverse(lst):
     while len(lst) > 0:
         result.append(lst.pop(len(lst)-1))
     return result
+
+## no need to specify pop index
+def reverse(lst):
+    result = list()
+    while lst:
+        result.append(lst.pop())
+    return result
