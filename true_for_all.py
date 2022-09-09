61426d4e158eccc594354fc1f443b97cc63e8382
Greg Mahone <gmahone@gmail.com>
2022-09-09 20:58:07 +0200

    feat: add loop-based solution

--- true_for_all.py
+++ true_for_all.py
@@ -1,2 +1,10 @@
 def _all(seq, fun): 
     return all(map(fun, seq))
+
+
+## solution using loop
+def all(seq, fun): 
+    for item in seq: 
+        if not fun(item): 
+            return False
+    return True
