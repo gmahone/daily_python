7b14ac511f8cb3dfc79bbb3ca63d215a7a9164aa
Greg Mahone <gmahone@gmail.com>
2022-09-12 20:43:55 +0200

    feat: add solution with init and property

--- classy_classes.py
+++ classy_classes.py
@@ -1,4 +1,13 @@
 class Person:
     def __init__(self, name, age):
         self.info="{name}s age is {age}".format(name = name, age = age)
+   
+## better separation
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
     
+    @property
+    def info(self):
+        return '{}s age is {}'.format(self.name, self.age)
