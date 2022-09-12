9ab896908b022ef67fc532a57a10e63d37df4e29
Greg Mahone <gmahone@gmail.com>
2022-09-12 20:44:38 +0200

    feat: add solution using f instead of format

--- classy_classes.py
+++ classy_classes.py
@@ -1,6 +1,11 @@
 class Person:
     def __init__(self, name, age):
         self.info="{name}s age is {age}".format(name = name, age = age)
+
+# add solution with f instead of format
+class Person:
+    def __init__(self, name, age):
+        self.info = f"{name}s age is {age}"
    
 ## better separation
 class Person:
