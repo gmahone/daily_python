b455466db702732c866388b1570c52073fa2132f
Greg Mahone <gmahone@gmail.com>
2022-09-14 15:47:50 +0200

    feat: add solution with direct string pasting

--- template_strings.py
+++ template_strings.py
@@ -4,3 +4,7 @@ def temple_strings(obj, feature):
 ## alternative with .format
 def temple_strings(obj, feature): 
     return "{} are {}".format(obj, feature)
+
+## alternative with direct string pasting
+def temple_strings(obj, feature): 
+    return obj + " are " + feature
