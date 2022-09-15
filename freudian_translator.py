88dcb48b14a4ddb007ed660eb24ffffd11565483
Greg Mahone <gmahone@gmail.com>
2022-09-15 20:50:10 +0200

    feat: add smaller solution

--- freudian_translator.py
+++ freudian_translator.py
@@ -1,2 +1,7 @@
 def to_freud(sentence):
   return " ".join(["sex" for i in (sentence.split(" "))]) if sentence else ""
+
+
+## smaller solution
+def to_freud(sentence):
+    return ' '.join('sex' for _ in sentence.split())
