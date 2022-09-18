cdf34ac082c3d840ed24e2c6499ddf89d837ba7e
Greg Mahone <gmahone@gmail.com>
2022-09-18 21:13:24 +0200

    fix: function name

--- object_oriented_piracy.py
+++ object_oriented_piracy.py
@@ -2,7 +2,7 @@ class Ship:
     def __init__(self, draft, crew):
         self.draft = draft
         self.crew = crew
-    def is_worth_id(self):
+    def is_worth_it(self):
         return self.draft - 1.5*self.crew > 20
         
 
