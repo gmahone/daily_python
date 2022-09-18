14d48d25a45c9dc787583429f6935ea329b29c31
Greg Mahone <gmahone@gmail.com>
2022-09-18 21:04:46 +0200

    feat: add solution using defaults for no if

--- max_diff.py
+++ max_diff.py
@@ -1,2 +1,7 @@
 def max_diff(lst):
     return max(lst) - min(lst) if lst else 0
+
+
+## similar solution using defaults
+def max_diff(lst):
+    return max(lst, default = 0) - min(lst, default = 0)
