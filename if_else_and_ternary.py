8c0bb16746467f420e3603ad1293c0e9b78a5357
Greg Mahone <gmahone@gmail.com>
2022-09-19 21:25:35 +0200

    fix: result value

--- if_else_and_ternary.py
+++ if_else_and_ternary.py
@@ -1,7 +1,7 @@
 def sale_hotdogs(n):
     if n >= 10:
-        return 90
+        return 90*n
     elif n < 5:
-        return 100
+        return 100*n
     else:
-        return 95
+        return 95*n
