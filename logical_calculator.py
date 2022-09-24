6ab541d664a1a92d8cfdc39fc22c4f48347f1d7b
Greg Mahone <gmahone@gmail.com>
2022-09-24 16:56:23 +0200

    fix: debugging XOR edge case

--- logical_calculator.py
+++ logical_calculator.py
@@ -1,7 +1,7 @@
 def test_xor(arr):
+    print(arr)
     if(len(arr) > 2):
-        print([arr[0] != arr[1]] + [arr[2:]])
-        test_xor([arr[0] != arr[1]] + [arr[2:]])
+        return test_xor([arr[0] != arr[1]] + arr[2:])
     else:
         return arr[0] != arr[1]
 
