937e7fa3f1c7dd1db5f0fa738fe2204074dd9f0e
Greg Mahone <gmahone@gmail.com>
2022-09-24 17:00:20 +0200

    feat: add solution directly using operators

--- logical_calculator.py
+++ logical_calculator.py
@@ -13,3 +13,17 @@ def logical_calc(array, op):
         return all(array)
     if op == "XOR":
         return test_xor(array)
+
+    
+    ## directly using operators
+    def logical_calc(array, op):
+    res = array[0]
+    for x in array[1:]:
+        if op == "AND":
+            res &= x
+        elif op == "OR":
+            res |= x
+        else:
+            res ^= x
+        
+    return res #boolean
