51d0d9dd45f7687422070e285e45758603472f1c
Greg Mahone <gmahone@gmail.com>
2022-09-24 17:01:53 +0200

    feat: add solution using reduce

--- logical_calculator.py
+++ logical_calculator.py
@@ -27,3 +27,10 @@ def logical_calc(array, op):
             res ^= x
         
     return res #boolean
+
+
+## using imported reduce
+from functools import reduce
+
+def logical_calc(array, op):
+    return reduce(lambda x,y: {'OR': x or y, 'AND': x and y, 'XOR': x ^ y}[op], array)
