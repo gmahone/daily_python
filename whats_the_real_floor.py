77342e8cadadbe163fa38e75512b58f80534605f
Greg Mahone <gmahone@gmail.com>
2022-09-25 20:57:55 +0200

    feat: add if case solution

--- whats_the_real_floor.py
+++ whats_the_real_floor.py
@@ -1,2 +1,8 @@
 def get_real_floor(n):
     return n-2 if n > 13 else n-1 if n > 0 else n
+
+# standard if case solution
+def get_real_floor(n):
+    if n <= 0: return n
+    if n < 13: return n-1
+    if n > 13: return n-2
