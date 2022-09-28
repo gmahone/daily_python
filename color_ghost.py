a6d504e9039cf06007e63465f8f6af0dbc124844
Greg Mahone <gmahone@gmail.com>
2022-09-28 21:42:42 +0200

    fix: added random class

--- color_ghost.py
+++ color_ghost.py
@@ -1,4 +1,4 @@
 class Ghost(object):
     def __init__(self):
         color_list = ["white", "yellow", "purple", "red"]
-        self.color = color_list[randint(0,3)]
+        self.color = color_list[random.randint(0,3)]
