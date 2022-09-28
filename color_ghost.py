9b7d5b0316f419c7f7cb405a231a23208c2ad901
Greg Mahone <gmahone@gmail.com>
2022-09-28 21:45:10 +0200

    feat: add solution using random.choice

--- color_ghost.py
+++ color_ghost.py
@@ -3,3 +3,10 @@ class Ghost(object):
     def __init__(self):
         color_list = ["white", "yellow", "purple", "red"]
         self.color = color_list[random.randint(0,3)]
+
+        
+## solution using random.choice
+import random
+class Ghost(object):
+  def __init__(self):
+    self.color = random.choice(["white", "yellow", "purple", "red"])
