c2080963db0e743696b608e8a8755ca4999867ae
Greg Mahone <gmahone@gmail.com>
2022-10-05 20:58:41 +0200

    feat: solution using get type

--- who_ate_the_cookie.py
+++ who_ate_the_cookie.py
@@ -6,3 +6,8 @@ def cookie(x):
     else:
         culprit = "the dog"
     return "Who ate the last cookie? It was {}!".format(culprit)
+
+
+# using get type
+def cookie(x):
+    return "Who ate the last cookie? It was %s!" % {str:"Zach", float:"Monica", int:"Monica"}.get(type(x), "the dog")
