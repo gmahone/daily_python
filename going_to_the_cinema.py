d93704fe45319967ed109f25675cabbbb0b407a9
Greg Mahone <gmahone@gmail.com>
2022-10-06 21:31:43 +0200

    fix: remove print

--- going_to_the_cinema.py
+++ going_to_the_cinema.py
@@ -2,7 +2,6 @@ def movie(card, ticket, perc):
     i = 1
     while True:
         card += (ticket * (perc**i))
-        print(round(card))
         if round(card) < (ticket*i):
             return i
         i += 1
