4bff1cb5a4734f8cedf40541f4d7639aea96fced
Greg Mahone <gmahone@gmail.com>
2022-10-06 21:27:58 +0200

    fix: initial logic

--- going_to_the_cinema.py
+++ going_to_the_cinema.py
@@ -1,7 +1,8 @@
 def movie(card, ticket, perc):
-    i <- 1
-    while true:
-        card += (ticket * (perc^i))
+    i = 1
+    while True:
+        card += (ticket * (perc**i))
         print(round(card))
         if i > 5:
             return "Stopped"
+        i += 1
