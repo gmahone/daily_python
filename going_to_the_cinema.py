7514028be509334c493f117e445ba6da566b51a6
Greg Mahone <gmahone@gmail.com>
2022-10-06 21:32:10 +0200

    fix: edge case of round, must use ceil

--- going_to_the_cinema.py
+++ going_to_the_cinema.py
@@ -1,7 +1,8 @@
+import math
 def movie(card, ticket, perc):
     i = 1
     while True:
         card += (ticket * (perc**i))
-        if round(card) < (ticket*i):
+        if math.ceil(card) < (ticket*i):
             return i
         i += 1
