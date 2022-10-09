beb8bbc5f316c72e4372c878935147ec023e1490
Greg Mahone <gmahone@gmail.com>
2022-10-09 20:17:26 +0200

    fix: conditional should be >=

--- human_cat_dog_years.py
+++ human_cat_dog_years.py
@@ -2,7 +2,7 @@ def human_years_cat_years_dog_years(human_years):
     result_human = human_years
     result_cat = 15
     result_dog = 15
-    if human_years > 2:
+    if human_years >= 2:
         result_cat += 9 + (4 * (human_years - 2))
         result_dog += 9 + (5 * (human_years - 2))
     return [result_human, result_cat, result_dog]
