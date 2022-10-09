2956399f7d7d6c08d00af6be784d91d2c10a909a
Greg Mahone <gmahone@gmail.com>
2022-10-09 20:18:24 +0200

    feat: add fully conditional solution

--- human_cat_dog_years.py
+++ human_cat_dog_years.py
@@ -6,3 +6,8 @@ def human_years_cat_years_dog_years(human_years):
         result_cat += 9 + (4 * (human_years - 2))
         result_dog += 9 + (5 * (human_years - 2))
     return [result_human, result_cat, result_dog]
+
+
+## fully conditional
+def human_years_cat_years_dog_years(x):
+    return [x, 24+(x-2)*4 if (x != 1) else 15, 24+(x-2)*5 if (x != 1) else 15]
