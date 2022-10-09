515749be6fb7e2592e8b6757632d576c0b9ef087
Greg Mahone <gmahone@gmail.com>
2022-10-09 18:22:22 +0200

    feat: working on logic of years > 2

--- human_cat_dog_years.py
+++ human_cat_dog_years.py
@@ -2,7 +2,9 @@ def human_years_cat_years_dog_years(human_years):
     result_human = human_years
     result_cat = 15
     result_dog = 15
-    if human_years == 1:
+    if human_years > 2:
+        result_cat += 9 + ()
+        result_dog += 9 + ()
 
     
     pass
