91eccfb631e3908e5d0479dbca2204f5792daf01
Greg Mahone <gmahone@gmail.com>
2022-10-09 18:23:20 +0200

    feat: initial solution

--- human_cat_dog_years.py
+++ human_cat_dog_years.py
@@ -3,8 +3,6 @@ def human_years_cat_years_dog_years(human_years):
     result_cat = 15
     result_dog = 15
     if human_years > 2:
-        result_cat += 9 + ()
-        result_dog += 9 + ()
-
-    
-    pass
+        result_cat += 9 + (4 * (human_years - 2))
+        result_dog += 9 + (5 * (human_years - 2))
+    return [result_human, result_cat, result_dog]
