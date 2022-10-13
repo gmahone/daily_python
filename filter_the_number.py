01a284565d597c6f98fc8ed93d8584298ee77768
Greg Mahone <gmahone@gmail.com>
2022-10-13 21:56:41 +0200

    fix: add digit test function

--- filter_the_number.py
+++ filter_the_number.py
@@ -1,5 +1,8 @@
+def digit_test(s):
+    return s.isdigit()
+
 def filter_string(string):
     string_list = list(string)
-    result = filter(isdigit, string_list)
+    result = filter(digit_test, string_list)
     print(result)
     return 0
