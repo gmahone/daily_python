669615befb929a4f062e4923e413acd9f5cc5222
Greg Mahone <gmahone@gmail.com>
2022-10-14 21:17:55 +0200

    feat: loop over words to split and join

--- methods_of_string_object.py
+++ methods_of_string_object.py
@@ -1,6 +1,6 @@
 def split_and_merge(string_, separator):
     words = string_.split(" ")
-    print(words)
+    paste([separator.join(list(c)) for c in word])        
     ##split_string = list(string_)
     ##print(separator.join(split_string))
     pass
