153f50189568bc59abe33ff236b4bfa23cb4f4b0
Greg Mahone <gmahone@gmail.com>
2022-10-14 21:21:41 +0200

    feat: add shorter solution

--- methods_of_string_object.py
+++ methods_of_string_object.py
@@ -2,3 +2,7 @@ def split_and_merge(string_, separator):
     words = string_.split(" ")
     result = " ".join([separator.join(list(c)) for c in words])        
     return result
+
+# shorter solution
+def split_and_merge(string, sp):
+    return ' '.join(sp.join(word) for word in string.split())
