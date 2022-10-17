d3a867403522dee894db0b87b590eb620165b2b2
Greg Mahone <gmahone@gmail.com>
2022-10-17 21:39:14 +0200

    feat: add second room

--- create_the_rooms.py
+++ create_the_rooms.py
@@ -1,4 +1,6 @@
 rooms = {"tomb": {"name": "Pharoah's Curse",
                   "description": "Egypt-themed",
                   "completed": False},
-         ""}
+         "cave": {"name": "Cave of Despare",
+                  "description": "Mine-themed",
+                  "completed": True},}
