c2dc4cbf7baa678804f824a3c77e59550122ba07
Greg Mahone <gmahone@gmail.com>
2022-10-18 22:28:26 +0200

    feat: simpler formulation of solution

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -10,3 +10,12 @@ def max_rot(n):
         n = before_index + after_index + index_value
         rot_list.append(int(n))
     return max(rot_list)
+
+
+# simpler formulation
+def max_rot(n):
+    s, arr = str(n), [n]
+    for i in range(len(s)):
+        s = s[:i] + s[i+1:] + s[i]
+        arr.append(int(s))
+    return max(arr)
