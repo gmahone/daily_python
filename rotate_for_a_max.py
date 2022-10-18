a23ed41fb6b20ad11187a81eca9173b91de4164b
Greg Mahone <gmahone@gmail.com>
2022-10-18 22:26:03 +0200

    fix: working on solution

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -4,11 +4,13 @@ def max_rot(n):
     for i in range(0,len(n)):
         after_index = n[i+1:]
         index_value = n[i:i+1]
-        before_index = n[:i]
+        if i is not 0:
+            before_index = n[:i]
+        else:
+            before_index = ""
         print("full n: {}".format(n))
         print("after_index: {}".format(after_index))
         print("index_value: {}".format(index_value))
         print("before_index: {}".format(before_index))
-        #if i is not 0:
-        #    tmp = n[:i-1] + tmp
+        n = before_index + after_index + index_value
     pass
