fa0bd15ddccf1dc3c7e7cc35fe65d9c24f2965d0
Greg Mahone <gmahone@gmail.com>
2022-10-18 22:17:29 +0200

    fix: dissecting string via indices

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -3,11 +3,12 @@ def max_rot(n):
     rot_list = []
     print(n)
     for i in range(0,len(n)):
-        print(n[i+1:])
-        print(n[i:i+1])
-        tmp = n[i+1:] + n[i:i+1]
+        after_index = n[i+1:]
+        index_value = n[i:i+1]
+        before_index = n[:i-1]
+        print(after_index)
+        print(index_value)
+        print(before_index)
         if i is not 0:
             tmp = n[:i-1] + tmp
-        print(tmp)
-        n = tmp
     pass
