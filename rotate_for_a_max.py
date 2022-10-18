fa64084d0a471a13d112643492d7f51b46fb7aa6
Greg Mahone <gmahone@gmail.com>
2022-10-18 21:52:32 +0200

    fix: looping and rotating

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -1,5 +1,8 @@
 def max_rot(n):
     n = str(n)
+    rot_list <- []
     print(n)
-    print([(n[slice(i+1, len(n))] + n[i]) for i in range(0,len(n))])
+    for i in range(0,len(n)):
+        rot_list.append(n[slice(i+1, len(n))] + n[i]))
+        print(rot_list)
     pass
