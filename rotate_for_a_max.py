6a76ce65efa74595d2179c8e8c206f65752ae41b
Greg Mahone <gmahone@gmail.com>
2022-10-18 22:26:56 +0200

    feat: rearrange and remove prints

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -4,14 +4,9 @@ def max_rot(n):
     for i in range(0,len(n)):
         after_index = n[i+1:]
         index_value = n[i:i+1]
+        before_index = ""
         if i is not 0:
             before_index = n[:i]
-        else:
-            before_index = ""
-        print("full n: {}".format(n))
-        print("after_index: {}".format(after_index))
-        print("index_value: {}".format(index_value))
-        print("before_index: {}".format(before_index))
         n = before_index + after_index + index_value
         rot_list.append(int(n))
     return max(rot_list)
