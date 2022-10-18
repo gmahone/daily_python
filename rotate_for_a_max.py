0e371abc286c4ee67148cea4df0bd815946f3447
Greg Mahone <gmahone@gmail.com>
2022-10-18 22:22:01 +0200

    fix: still working on rotation

--- rotate_for_a_max.py
+++ rotate_for_a_max.py
@@ -1,14 +1,14 @@
 def max_rot(n):
     n = str(n)
     rot_list = []
-    print(n)
     for i in range(0,len(n)):
         after_index = n[i+1:]
         index_value = n[i:i+1]
-        before_index = n[:i-1]
-        print(after_index)
-        print(index_value)
-        print(before_index)
-        if i is not 0:
-            tmp = n[:i-1] + tmp
+        before_index = n[:i]
+        print("full n: {}".format(n))
+        print("after_index: {}".format(after_index))
+        print("index_value: {}".format(index_value))
+        print("before_index: {}".format(before_index))
+        #if i is not 0:
+        #    tmp = n[:i-1] + tmp
     pass
