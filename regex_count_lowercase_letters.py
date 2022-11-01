17237ce7fb2aaa6a4ba45f3db3ffba43d0590700
Greg Mahone <gmahone@gmail.com>
2022-11-01 22:17:56 +0100

    feat: add list comprehension solution

--- regex_count_lowercase_letters.py
+++ regex_count_lowercase_letters.py
@@ -4,3 +4,8 @@ def lowercase_count(strng):
         if i.islower():
             result += 1
     return result
+
+
+# list comprehension
+def lowercase_count(strng):
+    return sum(a.islower() for a in strng)
