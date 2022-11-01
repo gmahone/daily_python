908e9c807bcac3b525577f48ec0f6b08335198a5
Greg Mahone <gmahone@gmail.com>
2022-11-01 22:18:21 +0100

    feat: add regex solution

--- regex_count_lowercase_letters.py
+++ regex_count_lowercase_letters.py
@@ -9,3 +9,9 @@ def lowercase_count(strng):
 # list comprehension
 def lowercase_count(strng):
     return sum(a.islower() for a in strng)
+
+
+# regex
+import re
+def lowercase_count(string):
+    return len(re.findall('[a-z]',string))
