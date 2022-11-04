806fd19d7576f68809289935ecd7a96c287720ef
Greg Mahone <gmahone@gmail.com>
2022-11-04 20:43:34 +0100

    feat: add solution using import re

--- contamination_1.py
+++ contamination_1.py
@@ -1,3 +1,9 @@
 def contamination(text, char):
   result_len = len(text)
   return char*result_len
+
+
+# using import re
+import re
+def contamination(text, char):
+  return re.sub(".", char, text)
