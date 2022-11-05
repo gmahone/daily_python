e43c5569a29d0be25de91f7ff54f275b550159ba
Greg Mahone <gmahone@gmail.com>
2022-11-05 08:58:34 +0100

    fix: strings in dict

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,4 +1,4 @@
 def calculate_tip(amount, rating):
-    tip_dict <- {terrible: 0, poor: 0.05, good: 0.1, great: 0.15, excellent: 0.2}
+    tip_dict <- {"terrible": 0, "poor": 0.05, "good": 0.1, "great": 0.15, "excellent": 0.2}
     print(tip_dict[rating.lower()])
     pass
