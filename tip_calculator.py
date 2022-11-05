7f1b05813d3bf0ee50340519299d595b815fda45
Greg Mahone <gmahone@gmail.com>
2022-11-05 09:04:16 +0100

    feat: add logic

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,4 +1,8 @@
 def calculate_tip(amount, rating):
     tip_dict = {"terrible": 0, "poor": 0.05, "good": 0.1, "great": 0.15, "excellent": 0.2}
-    print(tip_dict[rating.lower()])
+    try:
+        result = ceil(amount * tip_dict[rating.lower()])
+    except:
+        result = "Rating not recognized"
+    print(result)
     pass
