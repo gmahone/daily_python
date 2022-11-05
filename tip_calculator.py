0c14c74604073b256da7437154955e14eb48b6e7
Greg Mahone <gmahone@gmail.com>
2022-11-05 09:04:30 +0100

    fix: ceil call

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,7 +1,7 @@
 def calculate_tip(amount, rating):
     tip_dict = {"terrible": 0, "poor": 0.05, "good": 0.1, "great": 0.15, "excellent": 0.2}
     try:
-        result = ceil(amount * tip_dict[rating.lower()])
+        result = math.ceil(amount * tip_dict[rating.lower()])
     except:
         result = "Rating not recognized"
     print(result)
