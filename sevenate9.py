e154c23e97bf2a004686381df99163866ee7ce25
Greg Mahone <gmahone@gmail.com>
2022-11-08 20:24:42 +0100

    feat: add solution using find and replace

--- sevenate9.py
+++ sevenate9.py
@@ -3,3 +3,9 @@ def seven_ate9(str_):
     while "797" in result:
         result = "77".join(result.split("797"))
     return result
+
+# using find and replace
+def seven_ate9(str_):
+   while str_.find('797') != -1:
+       str_ = str_.replace('797','77')
+   return str_
