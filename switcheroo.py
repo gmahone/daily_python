0a753544fa7212aeaf2c7526e1c2c8ad521f31d4
Greg Mahone <gmahone@gmail.com>
2022-11-09 20:31:08 +0100

    fix: index from 1

--- switcheroo.py
+++ switcheroo.py
@@ -1,7 +1,7 @@
 def vowel_2_index(string):
     for vowel in "aeiou":
         while vowel in string:
-            tmp_index = string.index(vowel)
+            tmp_index = string.index(vowel) + 1
             print(tmp_index)
             string = string.replace(vowel, str(tmp_index), 1)
     pass
