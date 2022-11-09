51e38da5fa3ff10fd439c708004b53b260f5d935
Greg Mahone <gmahone@gmail.com>
2022-11-09 20:55:39 +0100

    feat: add short enumerate solution

--- switcheroo.py
+++ switcheroo.py
@@ -6,3 +6,9 @@ def vowel_2_index(string):
             string = string.replace(vowel, "x", 1)
             result = result.replace(vowel, str(tmp_index), 1)
     return result
+
+
+# short solution with enumerate
+def vowel_2_index(string):
+    vowels = 'aeiouAEIOU'
+    return ''.join(x if x not in vowels else str(n + 1) for n,x in enumerate(string))
