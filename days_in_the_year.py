1808f385c0535cce380a9c66f08fc936c0c61859
Greg Mahone <gmahone@gmail.com>
2022-11-10 21:27:42 +0100

    fix: conditional

--- days_in_the_year.py
+++ days_in_the_year.py
@@ -1,6 +1,6 @@
 def year_days(year):
     result = "{} has 365 days".format(year)
-    if year % 4:
+    if not year % 4:
         result = "{} has 366 days".format(year)
     return result
         
