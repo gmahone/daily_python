17bac4e9a441f93cbdd255d79d6e609e5b7e44a6
Greg Mahone <gmahone@gmail.com>
2022-11-10 21:34:54 +0100

    feat: add alternative solution

--- days_in_the_year.py
+++ days_in_the_year.py
@@ -5,4 +5,10 @@ def year_days(year):
     if not year % 100 and year % 400:
         result = "{} has 365 days".format(year)
     return result
-        
+
+# a different solution
+def year_days(year):
+    days = 365
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        days += 1
+    return "%d has %d days" % (year, days)
