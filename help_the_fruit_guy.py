fba8e23d074faf80df7b7616795fac393ef2ef82
Greg Mahone <gmahone@gmail.com>
2022-11-15 21:57:34 +0100

    feat: initial solution

--- help_the_fruit_guy.py
+++ help_the_fruit_guy.py
@@ -1,5 +1,4 @@
 def remove_rotten(bag_of_fruits):
     print(["rotten" in i for i in bag_of_fruits])
     result = [i.replace("rotten", "").lower() for i in bag_of_fruits]
-    print(result)
-    pass
+    return result
