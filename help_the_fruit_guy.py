741c247f9d2e138b37e8d4d85660e845574d0314
Greg Mahone <gmahone@gmail.com>
2022-11-15 22:04:04 +0100

    feat: add additional smaller solution

--- help_the_fruit_guy.py
+++ help_the_fruit_guy.py
@@ -3,3 +3,8 @@ def remove_rotten(bag_of_fruits):
         return []
     result = [i.replace("rotten", "").lower() for i in bag_of_fruits]
     return result
+
+
+## smaller solution
+def remove_rotten(bag_of_fruits):
+    return [x.replace('rotten', '').lower() for x in bag_of_fruits] if bag_of_fruits else []
