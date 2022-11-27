e13f562793d9c5ca35ff0e19d4ac3e70df513ff9
Greg Mahone <gmahone@gmail.com>
2022-11-27 21:42:18 +0100

    feat: remove print statements

--- basic_sequence_practice.py
+++ basic_sequence_practice.py
@@ -5,6 +5,4 @@ def sum_of_n(n):
     else:
         index_list = list(range(n,1))[::-1]
         result = [sum(index_list[0:n+1]) for n in list(range(0,-n+1))]
-    print(index_list)
-    print(result)
     return result
