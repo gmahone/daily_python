d51d1350c2b76e197d37d0802b8ffb69d4702b72
Greg Mahone <gmahone@gmail.com>
2022-11-27 21:41:47 +0100

    feat: working solution on tests

--- basic_sequence_practice.py
+++ basic_sequence_practice.py
@@ -4,7 +4,7 @@ def sum_of_n(n):
         result = [sum(index_list[0:n+1]) for n in index_list]
     else:
         index_list = list(range(n,1))[::-1]
-        result = [sum(index_list[0:n]) for n in list(range(0,n))]
+        result = [sum(index_list[0:n+1]) for n in list(range(0,-n+1))]
     print(index_list)
     print(result)
     return result
