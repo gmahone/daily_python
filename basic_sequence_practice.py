462a2bac1de329a7462960dbaa805b314f2836ac
Greg Mahone <gmahone@gmail.com>
2022-11-27 21:34:32 +0100

    feat: add initial return

--- basic_sequence_practice.py
+++ basic_sequence_practice.py
@@ -5,4 +5,6 @@ def sum_of_n(n):
         index_list = list(range(n,1))[::-1]
     #return [sum(range(0,n)) for n in range(0,n)]
     print(index_list)
+    result = [sum(index_list[0:n+1]) for n in index_list]
+    print(result)
     pass
