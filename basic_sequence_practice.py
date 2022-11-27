1aa08a59b0a738224670e10033d9935ef8ab96b7
Greg Mahone <gmahone@gmail.com>
2022-11-27 21:40:23 +0100

    fix: edge cases for negative n

--- basic_sequence_practice.py
+++ basic_sequence_practice.py
@@ -1,10 +1,10 @@
 def sum_of_n(n):
     if n >= 0:
         index_list = list(range(0,n+1))
+        result = [sum(index_list[0:n+1]) for n in index_list]
     else:
         index_list = list(range(n,1))[::-1]
-    #return [sum(range(0,n)) for n in range(0,n)]
+        result = [sum(index_list[0:n]) for n in list(range(0,n))]
     print(index_list)
-    result = [sum(index_list[0:n+1]) for n in index_list]
     print(result)
-    pass
+    return result
