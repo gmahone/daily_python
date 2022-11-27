1f995c6fad2f52a53b6455e641de6d258f12aba0
Greg Mahone <gmahone@gmail.com>
2022-11-27 21:42:53 +0100

    feat: add solution using xrange

--- basic_sequence_practice.py
+++ basic_sequence_practice.py
@@ -6,3 +6,8 @@ def sum_of_n(n):
         index_list = list(range(n,1))[::-1]
         result = [sum(index_list[0:n+1]) for n in list(range(0,-n+1))]
     return result
+
+
+# solution using xrange
+def sum_of_n(n):
+    return [(-1 if n < 0 else 1) * sum(xrange(i+1)) for i in xrange(abs(n)+1)]
