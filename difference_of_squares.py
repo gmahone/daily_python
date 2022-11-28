26971ee1f7324482edb7845375fe9c9321ffcf8e
Greg Mahone <gmahone@gmail.com>
2022-11-28 21:38:12 +0100

    feat: remove print statements

--- difference_of_squares.py
+++ difference_of_squares.py
@@ -3,9 +3,6 @@ def sqr(x):
 
 def difference_of_squares(n):
     n_vec = list(range(1,n+1))
-    print(n_vec)
     squared_sum = sum(n_vec)**2
-    print(squared_sum)
     sum_of_squares = sum(map(sqr, n_vec))
-    print(sum_of_squares)
     return squared_sum - sum_of_squares
