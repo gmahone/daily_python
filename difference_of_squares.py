0fd4eea124fa6666a238ba3b5c61b141673b97a5
Greg Mahone <gmahone@gmail.com>
2022-11-28 21:39:38 +0100

    feat: add solution with loop

--- difference_of_squares.py
+++ difference_of_squares.py
@@ -6,3 +6,8 @@ def difference_of_squares(n):
     squared_sum = sum(n_vec)**2
     sum_of_squares = sum(map(sqr, n_vec))
     return squared_sum - sum_of_squares
+
+
+# list comprehension
+def difference_of_squares(n):
+    return sum(range(n+1))**2 - sum(i**2 for i in range(n+1))
