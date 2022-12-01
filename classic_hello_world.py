ddebc466d824e19fa5c60095b9b25d9e02641803
Greg Mahone <gmahone@gmail.com>
2022-12-01 21:45:51 +0100

    feat: add solution using static method

--- classic_hello_world.py
+++ classic_hello_world.py
@@ -2,3 +2,10 @@ class Solution:
   def main(self):
     print("Hello World!")
     pass
+
+  
+# using static method
+class Solution:
+    @staticmethod
+    def main(self, *args):
+        print("Hello World!")
