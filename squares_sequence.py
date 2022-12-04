a207656a80d7e8a395a42b3752fe623395e6e9fb
Greg Mahone <gmahone@gmail.com>
2022-12-04 21:04:20 +0100

    fix: wrong variable used

--- squares_sequence.py
+++ squares_sequence.py
@@ -3,7 +3,7 @@ def squares(x: int, n: int) -> list[int]:
     if n <= 0:
         return result
     while len(result) < n:
-        result.append(n)
-        n = n*n
+        result.append(x)
+        x = x*x
     return result
     
