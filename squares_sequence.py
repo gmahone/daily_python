fdcf38ca09a47c03e56543a2aff7c12f239ff85e
Greg Mahone <gmahone@gmail.com>
2022-12-04 21:03:24 +0100

    feat: initial solution

--- squares_sequence.py
+++ squares_sequence.py
@@ -2,4 +2,8 @@ def squares(x: int, n: int) -> list[int]:
     result = []
     if n <= 0:
         return result
+    while len(result) < n:
+        result.append(n)
+        n = n*n
+    return result
     
