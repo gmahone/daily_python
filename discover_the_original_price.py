f60433c9f21e612c7745dcd72ef8fa27c6cd1892
Greg Mahone <gmahone@gmail.com>
2022-12-07 21:23:55 +0100

    feat: remove print statements

--- discover_the_original_price.py
+++ discover_the_original_price.py
@@ -1,9 +1,6 @@
 def discover_original_price(discounted_price, sale_percentage):
     sale_decimal = sale_percentage/100
-    print(sale_decimal)
     price_decimal = 1 - sale_decimal
-    print(price_decimal)
     result = discounted_price/price_decimal
-    print(result)
     rounded_result = round(result, 2)
     return rounded_result
