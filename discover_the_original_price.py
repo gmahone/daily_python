7eadc1674ca08b719e26d8e04592b48d4f31d662
Greg Mahone <gmahone@gmail.com>
2022-12-07 21:25:55 +0100

    feat: add alternative short solution

--- discover_the_original_price.py
+++ discover_the_original_price.py
@@ -9,3 +9,7 @@ def discover_original_price(discounted_price, sale_percentage):
 # short solution
 def discover_original_price(discounted_price, sale_percentage):
     return round(discounted_price / ((100 - sale_percentage) * 0.01), 2)
+
+# alternative short solution
+def discover_original_price(discounted_price, sale_percentage):
+    return round(discounted_price/(1-sale_percentage/100),2)
