f750098ba8876d0ff7841d93986a0d7ad1388dbf
Greg Mahone <gmahone@gmail.com>
2022-12-07 21:22:23 +0100

    feat: add debug solution

--- discover_the_original_price.py
+++ discover_the_original_price.py
@@ -1,2 +1,8 @@
 def discover_original_price(discounted_price, sale_percentage):
+    sale_decimal = sale_percentage/100
+    print(sale_decimal)
+    price_decimal = 1 - sale_decimal
+    print(price_decimal)
+    result = discounted_price/price_decimal
+    print(result)
     pass
