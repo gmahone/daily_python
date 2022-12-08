e6d67fb6ac69872f6ed3dbcd5de9230b9c174e9c
Greg Mahone <gmahone@gmail.com>
2022-12-08 20:38:20 +0100

    feat: add placeholder objects and functions

--- linked_lists_length_and_count.py
+++ linked_lists_length_and_count.py
@@ -1,1 +1,10 @@
-
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+def length(node):
+    # Your code goes here.
+  
+def count(node, data):
+    # Your code goes here.
