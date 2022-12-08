e24826fec82061ce85fe9043a477076b92ed2e0f
Greg Mahone <gmahone@gmail.com>
2022-12-08 21:03:41 +0100

    feat: add more concise solution

--- linked_lists_length_and_count.py
+++ linked_lists_length_and_count.py
@@ -24,3 +24,25 @@ def count(node, data):
         else:
             break
     return tmp_count
+
+
+## more concise solution
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+def length(node):
+    leng = 0
+    while node:
+        leng += 1
+        node = node.next
+    return leng
+  
+def count(node, data):
+    c = 0
+    while node:
+        if node.data==data:
+            c += 1
+        node = node.next
+    return c
