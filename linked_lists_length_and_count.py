a5a148a112170a5aef9d49fedb397ab73ad2e27e
Greg Mahone <gmahone@gmail.com>
2022-12-08 20:51:47 +0100

    feat: working on length function

--- linked_lists_length_and_count.py
+++ linked_lists_length_and_count.py
@@ -2,11 +2,15 @@ class Node(object):
     def __init__(self, data):
         self.data = data
         self.next = None
-    
+        
 def length(node):
     if node is None:
         return 0
-    print(node.items())
+    length = 0
+    while node.next is not None:
+        length += 1
+        node = node.next
+    print(length)
     pass
   
 def count(node, data):
