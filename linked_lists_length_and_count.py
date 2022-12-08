97aef8515aa28362ad6a594e26142b8bef7d0e4b
Greg Mahone <gmahone@gmail.com>
2022-12-08 20:46:55 +0100

    feat: working on object items

--- linked_lists_length_and_count.py
+++ linked_lists_length_and_count.py
@@ -4,7 +4,9 @@ class Node(object):
         self.next = None
     
 def length(node):
-    print(node)
+    if node is None:
+        return 0
+    print(node.items())
     pass
   
 def count(node, data):
