fe45f8ac207bbe26a3eef5bbf3287046dfdef598
Greg Mahone <gmahone@gmail.com>
2022-12-12 21:52:31 +0100

    feat: add lighter solution

--- looking_for_a_benefactor.py
+++ looking_for_a_benefactor.py
@@ -5,3 +5,13 @@ def new_avg(arr, newavg):
     if result < 0:
         raise ValueError
     return math.ceil(result)
+
+
+# import only ceil
+from math import ceil
+
+def new_avg(arr, newavg):
+    value = int(ceil((len(arr)+1) * newavg - sum(arr)))
+    if value < 0:
+        raise ValueError
+    return value
