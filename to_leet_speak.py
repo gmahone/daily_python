5e1083b301a23429cb1c80a3f2eff76ce711c35a
Greg Mahone <gmahone@gmail.com>
2022-12-15 20:39:27 +0100

    feat: add solution using translate and maketrans

--- to_leet_speak.py
+++ to_leet_speak.py
@@ -17,3 +17,8 @@ def to_leet_speak(str):
     for key, value in leet_dict.items():
         str = value.join(str.split(key))
     return str
+
+  
+# solution using translate
+def to_leet_speak(str):
+    return str.translate(str.maketrans("ABCEGHILOSTZ", "@8(36#!10$72"))
