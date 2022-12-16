a68be69717a9b456ab904bda4e6b775d26599453
Greg Mahone <gmahone@gmail.com>
2022-12-16 22:17:38 +0100

    feat: trying to create list of like chars

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,6 +1,9 @@
 def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
-    list_input = sorted(list_input)    
+    list_input = sorted(list_input)
+    tmp_list = list_input[1:2][0]
+    print(tmp_list)
+    print("  ")
     for i in range(len(list_input)-1):
         tmp_current = list_input[i]
         print(tmp_current)
