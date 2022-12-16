a798c2e4926d1b1948c7f6a6798098ad0715699a
Greg Mahone <gmahone@gmail.com>
2022-12-16 22:15:29 +0100

    feat: working on tool logic

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,6 +1,10 @@
 def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
-    list_input = sorted(list_input)
+    list_input = sorted(list_input)    
     for i in range(len(list_input)-1):
-        print(i)
+        tmp_current = list_input[i]
+        print(tmp_current)
+        tmp_next = list_input[i+1]
+        print(tmp_next)
+        print("  ")
     pass
