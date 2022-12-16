a26403af3dd779ccae7bb4da8172fc1559542981
Greg Mahone <gmahone@gmail.com>
2022-12-16 22:39:06 +0100

    fix: bug in initial indexing

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -4,14 +4,13 @@ def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
     list_input = sorted(list_input)
     tmp_list = []
-    curr_item = list_input[1:2][0]
+    curr_item = list_input[0:1][0]
     for i in range(len(list_input)-1):
+        print(curr_item)
         tmp_current = list_input[i]
         tmp_next = list_input[i+1]
         if(tmp_current == tmp_next):
             curr_item += tmp_next
-        elif i == 0:
-            curr_item = tmp_next
         else:
             tmp_list.append(curr_item)
             curr_item = tmp_next
