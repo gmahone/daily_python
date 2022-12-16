49eb94ba917412b7f50fc50586ff5db9bfaecd8f
Greg Mahone <gmahone@gmail.com>
2022-12-16 22:01:42 +0100

    feat: using standard loop over indices

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,12 +1,6 @@
 def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
     list_input = sorted(list_input)
-    iter_list = zip(list_input, list_input[1 : ] + list_input[ : 1])
-    iter_list = list(iter_list)
-    iter_list.pop()
-    print(iter_list)
-    for thiselem,nextelem in zip(list_input, list_input[1 : ] + list_input[ : 1]):
-        print(thiselem)
-        print(nextelem)
-        print("   ")
-    return []
+    for i in range(len(list_input)-1):
+        print(i)
+    pass
