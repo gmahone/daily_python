e61c860ae1baa2b5f495cb42e76f2d331f9740dc
Greg Mahone <gmahone@gmail.com>
2022-12-16 21:56:41 +0100

    feat: working on different ways to solve

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,4 +1,12 @@
 def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
     list_input = sorted(list_input)
-    return list_input
+    iter_list = zip(list_input, list_input[1 : ] + list_input[ : 1])
+    iter_list = list(iter_list)
+    iter_list.pop()
+    print(iter_list)
+    for thiselem,nextelem in zip(list_input, list_input[1 : ] + list_input[ : 1]):
+        print(thiselem)
+        print(nextelem)
+        print("   ")
+    return []
