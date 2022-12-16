abe1eec0fae3c38f9892029e92e642ae03570334
Greg Mahone <gmahone@gmail.com>
2022-12-16 21:43:00 +0100

    feat: convert to lower

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,4 +1,4 @@
 def find_children(dancing_brigade):
-    list_input = list(dancing_brigade)
+    list_input = list(dancing_brigade.lower())
     list_input = sorted(list_input)
     return list_input
