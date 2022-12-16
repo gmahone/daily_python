3d4393c2c7a1beee580f32791c369efb495a3084
Greg Mahone <gmahone@gmail.com>
2022-12-16 22:25:20 +0100

    feat: creating separate list items

--- where_is_my_parent.py
+++ where_is_my_parent.py
@@ -1,13 +1,15 @@
 def find_children(dancing_brigade):
     list_input = list(dancing_brigade.lower())
     list_input = sorted(list_input)
-    tmp_list = list_input[1:2][0]
-    print(tmp_list)
-    print("  ")
+    tmp_list = []
+    curr_item = list_input[1:2][0]
     for i in range(len(list_input)-1):
         tmp_current = list_input[i]
-        print(tmp_current)
         tmp_next = list_input[i+1]
-        print(tmp_next)
-        print("  ")
-    pass
+        if(tmp_current == tmp_next):
+            curr_item += tmp_next
+        else:
+            tmp_list.append(curr_item)
+            curr_item = tmp_next
+    tmp_list.append(curr_item)
+    print(tmp_list)
