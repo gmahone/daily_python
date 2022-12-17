b72f2d147aeb1fa017eb66835ffeb507cb96487a
Greg Mahone <gmahone@gmail.com>
2022-12-17 22:21:24 +0100

    feat: add alternative solution

--- seconds_to_hours_and_minutes.py
+++ seconds_to_hours_and_minutes.py
@@ -2,3 +2,8 @@ def to_time(seconds):
     hours = seconds // (60*60)
     minutes = seconds % (60*60) // 60
     return f"{hours} hour(s) and {minutes} minute(s)"
+
+
+# alternative
+def to_time(seconds):
+    return f'{seconds//3600} hour(s) and {(seconds//60)%60} minute(s)'
