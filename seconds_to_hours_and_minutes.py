ebc19e1e48cdda24324e07877b02741fbda371ae
Greg Mahone <gmahone@gmail.com>
2022-12-17 22:19:42 +0100

    feat: remove debug prints

--- seconds_to_hours_and_minutes.py
+++ seconds_to_hours_and_minutes.py
@@ -1,6 +1,4 @@
 def to_time(seconds):
     hours = seconds // (60*60)
     minutes = seconds % (60*60) // 60
-    print(hours)
-    print(minutes)
     return f"{hours} hour(s) and {minutes} minute(s)"
