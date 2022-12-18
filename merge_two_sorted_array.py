39d1e92b05bcf479da59849482bf56c0bcd4ce39
Greg Mahone <gmahone@gmail.com>
2022-12-18 20:50:48 +0100

    feat: sorted result

--- merge_two_sorted_array.py
+++ merge_two_sorted_array.py
@@ -4,6 +4,6 @@ def merge_arrays(arr1, arr2):
     for i in concat_list:
         if i not in unique_list:
             unique_list.append(i)
-    print(unique_list)
+    print(sorted(unique_list))
     pass
     
