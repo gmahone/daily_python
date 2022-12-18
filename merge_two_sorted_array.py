c53cc1a644d8244d81445df387dd4d7a59591b65
Greg Mahone <gmahone@gmail.com>
2022-12-18 20:51:26 +0100

    feat: initial solution

--- merge_two_sorted_array.py
+++ merge_two_sorted_array.py
@@ -4,6 +4,4 @@ def merge_arrays(arr1, arr2):
     for i in concat_list:
         if i not in unique_list:
             unique_list.append(i)
-    print(sorted(unique_list))
-    pass
-    
+    return sorted(unique_list)
