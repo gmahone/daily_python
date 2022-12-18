0b2807aad413ae0292f50e63d4796af2eae804f9
Greg Mahone <gmahone@gmail.com>
2022-12-18 20:49:18 +0100

    feat: find unique vals in concat list

--- merge_two_sorted_array.py
+++ merge_two_sorted_array.py
@@ -1,6 +1,9 @@
 def merge_arrays(arr1, arr2):
     unique_list = []
     concat_list = arr1 + arr2
-    print(concat_list)
+    for i in concat_list:
+        if i not in unique_list:
+            unique_list.append(i)
+    print(unique_list)
     pass
     
