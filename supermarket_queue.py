610f3b7cd3349c437eb205b1f15c1425226a1fd8
Greg Mahone <gmahone@gmail.com>
2023-01-08 22:05:22 +0100

    fix: max rather than min queue_list

--- supermarket_queue.py
+++ supermarket_queue.py
@@ -1,9 +1,7 @@
 def queue_time(customers, n):
-    if not customers:
-        return 0
     queue_list = [0] * n
     for customer in customers:
         shortest_length = min(queue_list)
         shortest_line = queue_list.index(shortest_length)
         queue_list[shortest_line] += customer
-    return min(queue_list)
+    return max(queue_list)
