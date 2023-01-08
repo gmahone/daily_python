b5a048bed90b4ea4b8832ec1652f9dc87ab3e2c0
Greg Mahone <gmahone@gmail.com>
2023-01-08 22:06:33 +0100

    feat: add shorter solution

--- supermarket_queue.py
+++ supermarket_queue.py
@@ -5,3 +5,11 @@ def queue_time(customers, n):
         shortest_line = queue_list.index(shortest_length)
         queue_list[shortest_line] += customer
     return max(queue_list)
+
+
+# more succinct ut less readable
+def queue_time(customers, n):
+    l=[0]*n
+    for i in customers:
+        l[l.index(min(l))]+=i
+    return max(l)
