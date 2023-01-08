9f4331c89c9ee2868caa34f2cef1c9d1d1d94db1
Greg Mahone <gmahone@gmail.com>
2023-01-08 21:59:28 +0100

    feat: add queue logic

--- supermarket_queue.py
+++ supermarket_queue.py
@@ -1,4 +1,8 @@
 def queue_time(customers, n):
     queue_list = [0] * n
+    for customer in customers:
+        shortest_length = min(queue_list)
+        shortest_line = queue_list.index(shortest_length)
+        queue_list[shortest_line] += customer
     print(queue_list)
     pass
