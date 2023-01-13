537df2f2f4cf71a7655c425f4eb48d3a37b57761
Greg Mahone <gmahone@gmail.com>
2023-01-13 16:07:19 +0100

    feat: rename dicts

--- alphabet_war.py
+++ alphabet_war.py
@@ -1,4 +1,4 @@
 def alphabet_war(fight):
-    left_score = {"w" : 4, "p" : 3, "b" : 2, "s" : 1}
-    right_score = {"m" : 4, "q" : 3, "d" : 2, "z" : 1}
+    left_score_dict = {"w" : 4, "p" : 3, "b" : 2, "s" : 1}
+    right_score_dict = {"m" : 4, "q" : 3, "d" : 2, "z" : 1}
     pass
