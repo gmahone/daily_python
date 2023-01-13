fddffaf67adc562ab40837bfeec29982eaecddc1
Greg Mahone <gmahone@gmail.com>
2023-01-13 16:09:07 +0100

    feat: define right and left side score variable

--- alphabet_war.py
+++ alphabet_war.py
@@ -1,4 +1,6 @@
 def alphabet_war(fight):
     left_score_dict = {"w" : 4, "p" : 3, "b" : 2, "s" : 1}
     right_score_dict = {"m" : 4, "q" : 3, "d" : 2, "z" : 1}
+    
+    left_side_score = right_side_score = 0
     pass
