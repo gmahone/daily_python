a2ad0ce5b6ce11b04ca63992356ab1541bbb912e
Greg Mahone <gmahone@gmail.com>
2023-01-14 09:20:50 +0100

    feat: initial solution

--- localize_the_barycenter.py
+++ localize_the_barycenter.py
@@ -1,3 +1,5 @@
 def bar_triang(point_a, point_b, point_c): 
     barycenter_x = (point_a[0] + point_b[0] + point_c[0])/3
     barycenter_y = (point_a[1] + point_b[1] + point_c[1])/3
+
+    return [barycenter_x, barycenter_y]
