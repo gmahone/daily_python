80737f5af1219374fb8ae830699b37e44d77e18e
Greg Mahone <gmahone@gmail.com>
2023-01-14 09:23:21 +0100

    feat: solution formatting

--- localize_the_barycenter.py
+++ localize_the_barycenter.py
@@ -1,5 +1,4 @@
 def bar_triang(point_a, point_b, point_c): 
     barycenter_x = round((point_a[0] + point_b[0] + point_c[0])/3, 4)
     barycenter_y = round((point_a[1] + point_b[1] + point_c[1])/3, 4)
-
     return [barycenter_x, barycenter_y]
