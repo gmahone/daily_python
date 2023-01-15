6f94aec04ff3c57c5a3e11f8fc14169aea22c9ee
Greg Mahone <gmahone@gmail.com>
2023-01-15 20:53:30 +0100

    feat: add solution with alternative dict building

--- numbers_to_letters.py
+++ numbers_to_letters.py
@@ -31,3 +31,13 @@ def switcher(arr):
         "29": " "
     }
     return "".join([translation_dict[i] for i in arr])
+
+
+# different dict building
+def switcher(arr):
+    d = {str(i): chr(123-i) for i in range(1,27)}
+    d.update({'27':'!'})
+    d.update({'28':'?'})
+    d.update({'29':' '})
+    d.update({'0':''})
+    return ''.join([d[str(i)] for i in arr])
