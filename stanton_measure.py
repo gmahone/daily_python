b8c20206d5e6ea1a4c471d9d442b7e14967ec184
Greg Mahone <gmahone@gmail.com>
2023-01-17 21:42:13 +0100

    feat: initial solution

--- stanton_measure.py
+++ stanton_measure.py
@@ -1,2 +1,10 @@
 def stanton_measure(arr):
-    pass
+    unique_list = []
+    max_count = 0
+    for i in arr:
+        for i not in unique_list:
+            unique_list.append(i)
+            tmp_count = arr.count(i)
+            if tmp_count > max_count:
+                max_count = tmp_count
+    return max_count
