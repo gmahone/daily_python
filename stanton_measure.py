3105f7e73b480e94c6afce9fdc2b49d20d4bb920
Greg Mahone <gmahone@gmail.com>
2023-01-17 21:42:58 +0100

    fix: conditional test for duplicate

--- stanton_measure.py
+++ stanton_measure.py
@@ -2,7 +2,7 @@ def stanton_measure(arr):
     unique_list = []
     max_count = 0
     for i in arr:
-        for i not in unique_list:
+        if i not in unique_list:
             unique_list.append(i)
             tmp_count = arr.count(i)
             if tmp_count > max_count:
