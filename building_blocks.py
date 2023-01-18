51ff9ea6bfaccd4427a7bd7adb797ab73cb3bb22
Greg Mahone <gmahone@gmail.com>
2023-01-18 21:56:13 +0100

    feat: add best practice solution

--- building_blocks.py
+++ building_blocks.py
@@ -21,3 +21,28 @@ class Block:
         left_right = 2 * self.width * self.height
         top_bottom = 2 * self.width * self.length
         return front_back + left_right + top_bottom
+
+    
+# best practice
+class Block:
+    def __init__(self, values):
+        self._width = values[0]
+        self._length = values[1]
+        self._height = values[2]
+    
+    def get_width(self):
+        return self._width
+
+    def get_length(self):
+        return self._length
+    
+    def get_height(self):
+        return self._height
+    
+    def get_volume(self):
+        return self.get_width()*self.get_length()*self.get_height()
+    
+    def get_surface_area(self):
+        return 2*(self.get_width()*self.get_height()
+                  + self.get_width()*self.get_length()
+                  + self.get_length()*self.get_height())
