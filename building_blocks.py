95f25663332f431aea7adcb21851e4edd5e7566c
Greg Mahone <gmahone@gmail.com>
2023-01-18 21:47:45 +0100

    feat: add get_ functions

--- building_blocks.py
+++ building_blocks.py
@@ -3,3 +3,12 @@ class Block:
         self.width = dims[0]
         self.length = dims[1]
         self.height = dims[2]
+        
+    def get_width(self):
+        return self.width
+    
+    def get_length(self):
+        return self.length
+    
+    def get_height(self):
+        return self.height
