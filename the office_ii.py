16a8ef1bbbac5f20bf83cce04e3354279eb2bd74
Greg Mahone <gmahone@gmail.com>
2023-01-19 21:37:43 +0100

    feat: add assessment score dict

--- the office_ii.py
+++ the office_ii.py
@@ -1,2 +1,14 @@
 def boredom(staff):
+    assessment_score = {
+        "accounts": 1,
+        "finance": 2,
+        "canteen": 10,
+        "regulation": 3,
+        "trading": 6,
+        "change": 6,
+        "IS": 8,
+        "retail": 5,
+        "cleaning": 4,
+        "pissing about": 25,    
+    }
     pass
