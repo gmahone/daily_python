bf953ddae0f5ddd77053c3fa86c2577b43109169
Greg Mahone <gmahone@gmail.com>
2023-01-19 21:44:58 +0100

    feat: add alternative solution

--- the office_ii.py
+++ the office_ii.py
@@ -20,3 +20,25 @@ def boredom(staff):
         return "i can handle this"
     else:
         return "kill me now"
+
+    
+# alternative solution
+def boredom(staff):
+    lookup = {
+        "accounts": 1,
+        "finance": 2,
+        "canteen": 10,
+        "regulation": 3, 
+        "trading": 6,
+        "change": 6,
+        "IS": 8,
+        "retail": 5,
+        "cleaning": 4,
+        "pissing about": 25
+    }
+    n = sum(lookup[s] for s in staff.values())
+    if n <= 80:
+        return "kill me now"
+    if n < 100:
+        return "i can handle this"
+    return "party time!!"
