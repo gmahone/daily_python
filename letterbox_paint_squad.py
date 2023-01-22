9942d817c78305006a4d1d99abb83319638a8e42
Greg Mahone <gmahone@gmail.com>
2023-01-22 21:34:20 +0100

    feat: add alternative solution

--- letterbox_paint_squad.py
+++ letterbox_paint_squad.py
@@ -5,3 +5,12 @@ def paint_letterboxes(start, finish):
         concat_numbers += str(i)
     result = [concat_numbers.count(str(n)) for n in range(0,10)]
     return result
+
+
+# alternative solution
+def paint_letterboxes(start, finish):
+    xs = [0] * 10
+    for n in range(start, finish+1):
+        for i in str(n):
+            xs[int(i)] += 1
+    return xs
