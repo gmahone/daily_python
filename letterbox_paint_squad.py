213c9678cadf59df41a5d4864e51ade91deba7d1
Greg Mahone <gmahone@gmail.com>
2023-01-22 21:30:24 +0100

    feat: add number to long string logic

--- letterbox_paint_squad.py
+++ letterbox_paint_squad.py
@@ -1,4 +1,7 @@
 def paint_letterboxes(start, finish):
     number_list = list(range(start, finish+1))
-    print(number_list)
+    concat_numbers = ""
+    for i in number_list:
+        concat_numbers += str(i)
+    print(concat_numbers)
     pass
