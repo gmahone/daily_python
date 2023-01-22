cc53168b23a70cedf15e74edbb0374f356f90db0
Greg Mahone <gmahone@gmail.com>
2023-01-22 21:44:22 +0100

    fix: conditional missing

--- remove_all_marked_elements.py
+++ remove_all_marked_elements.py
@@ -2,6 +2,6 @@ class List:
     def remove_(self, integer_list, values_list):
         result = []
         for i in integer_list:
-            for i not in values_list:
+            if i not in values_list:
                 result.append(i)
         return result
