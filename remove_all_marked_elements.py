b33406a5bc222d709e9f0118c8c1a5eb8a4613b3
Greg Mahone <gmahone@gmail.com>
2023-01-22 21:45:35 +0100

    feat: add one line solution

--- remove_all_marked_elements.py
+++ remove_all_marked_elements.py
@@ -5,3 +5,8 @@ class List:
             if i not in values_list:
                 result.append(i)
         return result
+
+# one line solution
+class List(object):
+    def remove_(self, integer_list, values_list):
+        return [i for i in integer_list if i not in values_list]
