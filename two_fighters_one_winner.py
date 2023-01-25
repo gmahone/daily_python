7226fb327bb9245f43c9554d9aa14e70238d77f5
Greg Mahone <gmahone@gmail.com>
2023-01-25 21:53:18 +0100

    feat: add another solution

--- two_fighters_one_winner.py
+++ two_fighters_one_winner.py
@@ -9,3 +9,11 @@ def declare_winner(fighter1, fighter2, first_attacker):
         return fighter2.name
     else:
         return first_attacker
+
+# another solution
+def declare_winner(fighter1, fighter2, first_attacker):
+    cur, opp = (fighter1, fighter2) if first_attacker == fighter1.name else (fighter2, fighter1)
+    while cur.health > 0:        
+        opp.health -= cur.damage_per_attack
+        cur, opp = opp, cur
+    return opp.name
