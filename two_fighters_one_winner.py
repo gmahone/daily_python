a657a701a559f4c0f8b13487fbd00f036f80841f
Greg Mahone <gmahone@gmail.com>
2023-01-25 21:49:00 +0100

    fix: ceil scope

--- two_fighters_one_winner.py
+++ two_fighters_one_winner.py
@@ -1,8 +1,8 @@
 import math
 
 def declare_winner(fighter1, fighter2, first_attacker):
-    fighter1_turns = ceil(fighter1.health / fighter2.damage_per_attack))
-    fighter2_turns = ceil(fighter2.health / fighter1.damage_per_attack))
+    fighter1_turns = math.ceil(fighter1.health / fighter2.damage_per_attack)
+    fighter2_turns = math.ceil(fighter2.health / fighter1.damage_per_attack)
     print(fighter1_turns)
     print(fighter2_turns)
     pass
