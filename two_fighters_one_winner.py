e91187767db68471a14a66f808515cad6efa6956
Greg Mahone <gmahone@gmail.com>
2023-01-25 21:52:08 +0100

    feat: initial solution

--- two_fighters_one_winner.py
+++ two_fighters_one_winner.py
@@ -3,6 +3,9 @@ import math
 def declare_winner(fighter1, fighter2, first_attacker):
     fighter1_turns = math.ceil(fighter1.health / fighter2.damage_per_attack)
     fighter2_turns = math.ceil(fighter2.health / fighter1.damage_per_attack)
-    print(fighter1_turns)
-    print(fighter2_turns)
-    pass
+    if fighter1_turns > fighter2_turns:
+        return fighter1.name
+    elif fighter2_turns > fighter1_turns:
+        return fighter2.name
+    else:
+        return first_attacker
