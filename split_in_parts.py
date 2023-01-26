b99cbc6d5be66920aa745961bf92ffa61d97fce4
Greg Mahone <gmahone@gmail.com>
2023-01-26 22:08:15 +0100

    fix: variable in range

--- split_in_parts.py
+++ split_in_parts.py
@@ -1,4 +1,4 @@
 def split_in_parts(s, part_length):
-    result = [s[n:n+part_length] for n in range(0,n,part_length)]
+    result = [s[n:n+part_length] for n in range(0,len(s),part_length)]
     print(result)
     pass
