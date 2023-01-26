b54fff28863aa27234476b188ef130cdff44a6ac
Greg Mahone <gmahone@gmail.com>
2023-01-26 22:11:01 +0100

    feat: add single line solution

--- split_in_parts.py
+++ split_in_parts.py
@@ -1,3 +1,8 @@
 def split_in_parts(s, part_length):
     result = [s[n:n+part_length] for n in range(0,len(s),part_length)]
     return " ".join(result)
+
+
+# single line
+def split_in_parts(s, n): 
+    return ' '.join([s[i:i+n] for i in range(0, len(s), n)])
