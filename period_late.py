828494d98e8955f6a154371e9f1cbcd954f8788b
Greg Mahone <gmahone@gmail.com>
2023-01-29 21:39:29 +0100

    feat: add shorter solution

--- period_late.py
+++ period_late.py
@@ -1,3 +1,7 @@
 def period_is_late(last,today,cycle_length):
     lag = today - last
     return lag.days > cycle_length
+
+# shorter solution
+def period_is_late(last, today, cycle_length):
+    return (today - last).days > cycle_length
