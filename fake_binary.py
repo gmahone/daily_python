fe3df0484a3ccac6173eecc05f9ee2c2120ae9e2
Greg Mahone <gmahone@gmail.com>
2023-01-30 22:22:51 +0100

    feat: add solution with no type conversion

--- fake_binary.py
+++ fake_binary.py
@@ -1,3 +1,8 @@
 def fake_bin(x):
     result = ["0" if int(i) < 5 else "1" for i in x]
     return "".join(result)
+
+
+# with no conversion
+def fake_bin(x):
+    return ''.join('0' if c < '5' else '1' for c in x)
