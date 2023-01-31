534bdb46cfd5c05f3ddaa0a2447d7036bbb3ef55
Greg Mahone <gmahone@gmail.com>
2023-01-31 20:42:54 +0100

    feat: function logic

--- lottery_ticket.py
+++ lottery_ticket.py
@@ -1,2 +1,9 @@
 def bingo(ticket,win):
+    mini_wins = 0
+    for i in ticket:
+        for j in i[0]:
+            if ord(j) == i[1]:
+                mini_wins += 1
+                break
+        print(mini_wins)
     pass
