b9ff5171c9c2ab0d0fc3983eee5357a3079879b5
Greg Mahone <gmahone@gmail.com>
2023-01-31 21:19:11 +0100

    feat: return logic

--- products_of_array_maximums.py
+++ products_of_array_maximums.py
@@ -2,5 +2,4 @@ from math import prod
 
 def max_product(lst, n_largest_elements):
     largest_elements = sorted(lst, reverse=True)[0:n_largest_elements]
-    print(prod(largest_elements))
-    pass
+    return prod(largest_elements)
