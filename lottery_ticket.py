1cc830d5ddb4ddbd5318699dd1c82fe1e3c9efb0
Greg Mahone <gmahone@gmail.com>
2023-01-31 20:45:32 +0100

    feat: add single line solution

--- lottery_ticket.py
+++ lottery_ticket.py
@@ -6,3 +6,8 @@ def bingo(ticket,win):
                 mini_wins += 1
                 break
     return "Winner!" if mini_wins >= win else "Loser!"
+
+
+# single line
+def bingo(ticket, win):
+    return 'Winner!' if sum(chr(n) in s for s, n in ticket) >= win else 'Loser!'
