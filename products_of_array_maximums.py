2e0a2f0a0e6a514dab80adb4f284c77723ae4744
Greg Mahone <gmahone@gmail.com>
2023-01-31 21:18:35 +0100

    feat: add prod logic

--- products_of_array_maximums.py
+++ products_of_array_maximums.py
@@ -1,3 +1,6 @@
+from math import prod
+
 def max_product(lst, n_largest_elements):
-    print(sorted(lst, reverse=True)[0:(n_largest_elements-1)])
+    largest_elements = sorted(lst, reverse=True)[0:n_largest_elements]
+    print(prod(largest_elements))
     pass
