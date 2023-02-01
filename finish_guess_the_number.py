d28867ea99711044e14be233b194805dbfdcbbd8
Greg Mahone <gmahone@gmail.com>
2023-02-01 21:38:44 +0100

    feat: add boolean logic

--- finish_guess_the_number.py
+++ finish_guess_the_number.py
@@ -4,4 +4,9 @@ class Guesser:
         self.lives = lives
   
     def guess(self,n):
-        return False
+        if n == self.number:
+            return True
+        else:
+            self.lives -= 1
+            return False
+            
