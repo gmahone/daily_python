19c05bec101bafae281b99de5423c58cde4471e6
Greg Mahone <gmahone@gmail.com>
2023-02-02 21:45:50 +0100

    feat: add one line replace solution

--- safen_user_input_1.py
+++ safen_user_input_1.py
@@ -9,3 +9,8 @@ def html_special_chars(data):
 def html_special_chars(data): 
     symbols = {'<': '&lt;', '>': '&gt;', '"': '&quot;', '&': '&amp;'}
     return "".join(symbols.get(x, x) for x in data)
+
+
+# one line replace
+def html_special_chars(data): 
+    return data.replace('&', "&amp;").replace('>', "&gt;").replace('<', "&lt;").replace('\"', "&quot;")
