a7bfd8c15eb67d89a603a13023e1eae0d89ea1be
Greg Mahone <gmahone@gmail.com>
2023-02-02 22:09:27 +0100

    feat: add single line solution

--- if_you_can_read_this.py
+++ if_you_can_read_this.py
@@ -2,3 +2,7 @@ def to_nato(words):
     words = "".join(words.split(" "))
     result = " ".join(NATO.get(x.upper(), x) for x in words)
     return result
+
+# single line
+def to_nato(words):
+    return ' '.join(NATO.get(char, char) for char in words.upper() if char != ' ')
