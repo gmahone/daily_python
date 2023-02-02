77c99e16083c290bf4a73b1c0f87da1af7032bc6
Greg Mahone <gmahone@gmail.com>
2023-02-02 21:44:23 +0100

    fix: order of replacements

--- safen_user_input_1.py
+++ safen_user_input_1.py
@@ -1,6 +1,6 @@
 def html_special_chars(data):
+    data = data.replace("&", "&amp;")
     data = data.replace("<", "&lt;")
     data = data.replace(">", "&gt;")
     data = data.replace('"', '&quot;')
-    data = data.replace("&", "&amp;")
     return data
