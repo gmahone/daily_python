23f16f021ea3408033be4ddb0d56dc5ff4ca9976
Greg Mahone <gmahone@gmail.com>
2023-02-02 21:45:22 +0100

    feat: add solution using translation dict

--- safen_user_input_1.py
+++ safen_user_input_1.py
@@ -4,3 +4,8 @@ def html_special_chars(data):
     data = data.replace(">", "&gt;")
     data = data.replace('"', '&quot;')
     return data
+
+# using dict
+def html_special_chars(data): 
+    symbols = {'<': '&lt;', '>': '&gt;', '"': '&quot;', '&': '&amp;'}
+    return "".join(symbols.get(x, x) for x in data)
