ceff719ab8884ac22d1ec168209fe524b5951056
Greg Mahone <gmahone@gmail.com>
2023-02-03 21:47:03 +0100

    feat: add solution using ord

--- consonant_value.py
+++ consonant_value.py
@@ -38,3 +38,18 @@ def solve(s):
         score = [score_dict.get(consonant) for consonant in consonant_group]
         result_list.append(sum(score))
     return max(result_list)
+
+
+# solution with ord
+def solve(s):
+    count = 0
+    max = 0
+    vowels = ["a", "e", "i", "o", "u"]
+    for letter in s:
+        if letter in vowels:
+            if count > max:
+                max = count
+            count = 0
+        else:
+            count+=int(ord(letter)) - 96
+    return max
