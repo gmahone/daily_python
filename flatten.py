a6546a873f81a5d56637928fe1c5648a73968596
Greg Mahone <gmahone@gmail.com>
2023-02-05 22:12:21 +0100

    fix: solution formatting

--- flatten.py
+++ flatten.py
@@ -1,8 +1,6 @@
 def flatten(lst):
-    result = []
     try:
         result = [i for sublst in lst for i in sublst]
     except:
         result = lst
-    print(result)
-    pass
+    return result
