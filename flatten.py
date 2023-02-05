631950e5a5866ec055f95f17b45ffbe63d4de16d
Greg Mahone <gmahone@gmail.com>
2023-02-05 22:11:33 +0100

    feat: possible solution with try

--- flatten.py
+++ flatten.py
@@ -1,4 +1,8 @@
 def flatten(lst):
-    result = [i for sublst in lst for i in sublst]
+    result = []
+    try:
+        result = [i for sublst in lst for i in sublst]
+    except:
+        result = lst
     print(result)
     pass
