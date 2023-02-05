5abbc020ffc9d6c976d26cf35790a2a41cd9c371
Greg Mahone <gmahone@gmail.com>
2023-02-05 22:17:07 +0100

    feat: add another solution that fails random tests

--- flatten.py
+++ flatten.py
@@ -1,6 +1,17 @@
+# fails random tests
 def flatten(lst):
     try:
         result = [i for sublst in lst for i in sublst]
     except:
+        result = lst
+    return result
+
+# another solution that fails random tests
+import itertools
+
+def flatten(lst):
+    try:
+        result = list(itertools.chain(*lst))
+    except: 
         result = lst
     return result
