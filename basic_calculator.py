b297e1ecb174dbcd3f5dbaf8037fc55ce756aa7a
Greg Mahone <gmahone@gmail.com>
2023-02-06 21:43:57 +0100

    feat: add solution using a dict and lambda functions

--- basic_calculator.py
+++ basic_calculator.py
@@ -21,3 +21,10 @@ def calculate(num1, operation, num2):
         return eval("{} {} {}".format(num1, operation, num2))
     except (ZeroDivisionError, SyntaxError):
         return None
+
+    
+# using a dict and lambda functions
+def calculate(num1, operation, num2):
+    ops = {"+": (lambda x, y: x + y), "-": (lambda x, y: x - y), "*": (lambda x, y: x * y), "/": (lambda x, y: x / y)}
+    try: return ops[operation](num1, num2)
+    except: return None
