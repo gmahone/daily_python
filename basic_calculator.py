81a92d8a27219b0a8561670df9e9982955e12bd3
Greg Mahone <gmahone@gmail.com>
2023-02-06 21:38:41 +0100

    feat: function logic

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,2 +1,13 @@
-def calculate(num1, operation, num2): 
+def calculate(num1, operation, num2):
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        result = num1 / num2
+    else:
+        result = None
+    print(result)
     pass
