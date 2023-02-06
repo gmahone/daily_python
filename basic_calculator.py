549826cfe2cc0df48030468aed0569842b33b3df
Greg Mahone <gmahone@gmail.com>
2023-02-06 21:42:48 +0100

    feat: add solution using eval

--- basic_calculator.py
+++ basic_calculator.py
@@ -13,3 +13,11 @@ def calculate(num1, operation, num2):
     else:
         result = None
     return result
+
+
+# using eval
+def calculate(num1, operation, num2): 
+    try :
+        return eval("{} {} {}".format(num1, operation, num2))
+    except (ZeroDivisionError, SyntaxError):
+        return None
