37408cc80f52e20d7a5ad5036e5a26cd5c08c8a9
Greg Mahone <gmahone@gmail.com>
2023-02-07 22:20:30 +0100

    feat: add result logic

--- ipv4_to_int32.py
+++ ipv4_to_int32.py
@@ -2,4 +2,5 @@ def ip_to_int32(ip):
   split_ip = ip.split(".")
   bin_ip = [bin(int(i)).replace("0b", "") for i in split_ip]
   total_bin = "".join([i.zfill(8) for i in bin_ip])
+  print(int(total_bin, 2))
   pass
