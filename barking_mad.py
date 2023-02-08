62c99b8f4ad5357dd9bf8c8506d0777ff190ca62
Greg Mahone <gmahone@gmail.com>
2023-02-08 21:51:46 +0100

    feat: add solution using staticmethod

--- barking_mad.py
+++ barking_mad.py
@@ -9,3 +9,17 @@ class Dog ():
 snoopy = Dog("Beagle")
 
 scoobydoo = Dog("Great Dane")
+
+
+# using staticmethod
+class Dog ():
+    def __init__(self, breed):
+        self.breed = breed
+    
+    @staticmethod
+    def bark():
+        return "Woof"
+    
+snoopy = Dog("Beagle")
+
+scoobydoo = Dog("Great Dane")
