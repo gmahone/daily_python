8ac19c6313f8d097a268b0d80a8b2d04f6af003b
Greg Mahone <gmahone@gmail.com>
2023-02-12 21:30:34 +0100

    feat: add solution on single line

--- building_strings_from_hash.py
+++ building_strings_from_hash.py
@@ -2,3 +2,8 @@ def solution(pairs):
     result = [f"{key} = {value}" for key,value in pairs.items()]
     result = sorted(result)
     return ",".join(result)
+
+
+# single line solution
+def solution(pairs):
+    return ','.join(sorted([ f'{key} = {value}' for key,value in pairs.items() ]))
