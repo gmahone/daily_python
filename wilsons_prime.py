13d460a456c6f57e033068d448db7fe0bb94e1ef
Greg Mahone <gmahone@gmail.com>
2023-02-16 21:59:43 +0100

    feat: meme answer

--- wilsons_prime.py
+++ wilsons_prime.py
@@ -1,7 +1,3 @@
-import math
-
 def am_i_wilson(n):
-    if n < 2:
-        return False
-    result = (math.factorial(n-1) + 1) / (n * n)
-    return result == int(result)
+    wilson_primes = [5,13,563]
+    return n in wilson_primes
