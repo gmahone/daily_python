b8c680f826ab3f6dd6a59bd15200ff5357c0d542
Greg Mahone <gmahone@gmail.com>
2023-02-19 22:07:03 +0100

    feat: initial solution

--- ball_upwards.py
+++ ball_upwards.py
@@ -1,16 +1,17 @@
 def calc_height(v, t):
-    return v * t - 0.5 * 9.81 * t * t
+    return (v * t) - (0.5 * 9.81 * t * t)
     
 
 def max_ball(v0):
-    v0 = v0 * 1000 / 60 * 60
+    v0 = v0 * 1000 / 3600
     print(v0)
     height = 0
-    time = 0
+    time = 0.0
     while True:
-        curr_height = calc_height(v0, time)
         time += 0.1
+        curr_height = calc_height(v0, time)
+        print(curr_height)
         if curr_height > height:
             height = curr_height
         else:
-            return height
+            return round((time-0.1)*10)
