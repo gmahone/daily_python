9cf0b5c818d1b520d05c2cd10426339ed7f1634b
Greg Mahone <gmahone@gmail.com>
2023-02-19 21:47:45 +0100

    feat: working on solution

--- ball_upwards.py
+++ ball_upwards.py
@@ -1,4 +1,16 @@
+def calc_height(v, t):
+    return v * t - 0.5 * 9.81 * t * t
+    
+
 def max_ball(v0):
     v0 = v0 * 1000 / 60 * 60
     print(v0)
-    pass
+    height = 0
+    time = 0
+    while True:
+        curr_height = calc_height(v0, time)
+        time += 0.1
+        if curr_height > height:
+            height = curr_height
+        else:
+            return height
