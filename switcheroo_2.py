ff78e93228dea2b90dfc1af6154914f2b8cc1dd1
Greg Mahone <gmahone@gmail.com>
2023-02-20 22:02:00 +0100

    feat: add solution using translate

--- switcheroo_2.py
+++ switcheroo_2.py
@@ -3,3 +3,7 @@ def switcheroo(s):
     s = "a".join(s.split("b"))
     s = "b".join(s.split("_"))
     return s
+
+# using translate
+def switcheroo(s):
+    return s.translate(str.maketrans('ab','ba'))
