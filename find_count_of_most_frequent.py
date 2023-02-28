14358e5957c0f3a2eacca6ba7e58b9dac0aa17ca
Greg Mahone <gmahone@gmail.com>
2023-02-28 21:56:39 +0100

    feat: add initial solution

--- find_count_of_most_frequent.py
+++ find_count_of_most_frequent.py
@@ -1,2 +1,7 @@
 def most_frequent_item_count(collection):
-    pass
+    result_max = 0
+    for i in collection:
+        tmp_count = collection.count(i)
+        if tmp_count > result_max:
+            result_max = tmp_count
+    return result_max
