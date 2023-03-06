5067c3c5998cc8643f42767b6d115111c7acf9bb
Greg Mahone <gmahone@gmail.com>
2023-03-06 20:35:12 +0100

    feat: add solution with ternary

--- shorter_concat.py
+++ shorter_concat.py
@@ -12,3 +12,8 @@ def shorter_reverse_longer(a,b):
 def shorter_reverse_longer(a,b):
   if len(a) < len(b): a, b = b, a
   return b+a[::-1]+b
+
+
+# ternary
+def shorter_reverse_longer(a,b):
+    return b + a[::-1] + b if len(a) >= len(b) else a + b[::-1] + a
