0c38bb72d411b480603f9bdc181e71801d956db1
Greg Mahone <gmahone@gmail.com>
2023-03-06 20:34:41 +0100

    feat: add another formulation

--- shorter_concat.py
+++ shorter_concat.py
@@ -6,3 +6,9 @@ def shorter_reverse_longer(a,b):
     shorter = a
   result = shorter + longer[::-1] + shorter
   return result
+
+
+# multi assign
+def shorter_reverse_longer(a,b):
+  if len(a) < len(b): a, b = b, a
+  return b+a[::-1]+b
