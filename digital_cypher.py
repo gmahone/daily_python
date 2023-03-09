6ab4e9a63e3e8d381fec5ef980a31870725a5bff
Greg Mahone <gmahone@gmail.com>
2023-03-09 21:04:03 +0100

    feat: working on logic

--- digital_cypher.py
+++ digital_cypher.py
@@ -1,7 +1,15 @@
-from itertools import cycle
-
 def encode(message, key):
-    code_vec = "_abcdefghijklmnopqrstuvwxyz"
-    key_cycle = cycle(list(map(int, list(str(key)))))
-    #for i in message:
+    code_string = "_abcdefghijklmnopqrstuvwxyz"
+    key_cycle = list(map(int, list(str(key))))*len(message)
+    result_list = []
+    for i in range(0,len(message)):
+        base_val = code_string.index(message[i])
+        key_val = key_cycle[i]
+        result_list.append(base_val + key_val)
+        print(base_val)
+        print(key_val)
+        print(result_list)
+    pass
+                           
+        
         
