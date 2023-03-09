d01a1d6c06d8a5ca64179d23e1ec5299a5223052
Greg Mahone <gmahone@gmail.com>
2023-03-09 20:55:04 +0100

    feat: add code vec

--- digital_cypher.py
+++ digital_cypher.py
@@ -1,6 +1,7 @@
 from itertools import cycle
 
 def encode(message, key):
+    code_vec = "_abcdefghijklmnopqrstuvwxyz"
     key_cycle = cycle(list(map(int, list(str(key)))))
     #for i in message:
         
