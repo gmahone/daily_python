e49e1315d9939816364f24d399245b893bf2bf70
Greg Mahone <gmahone@gmail.com>
2023-03-12 21:14:47 +0100

    feat: add regexp solution

--- is_it_a_vowel.py
+++ is_it_a_vowel.py
@@ -7,3 +7,10 @@ def is_vowel(s):
 # solution using set
 def is_vowel(s):
     return s.lower() in set("aeiou")
+
+
+#using regexp
+import re
+def is_vowel(s): 
+    regex = r'^[aeiou]{1}$'
+    return bool(re.match(regex, re.escape(s), re.IGNORECASE))
