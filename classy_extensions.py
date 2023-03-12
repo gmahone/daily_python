327a9bdaba8da0937de588ab7d0885bc5ffd4ae4
Greg Mahone <gmahone@gmail.com>
2023-03-12 21:07:47 +0100

    feat: add another solution

--- classy_extensions.py
+++ classy_extensions.py
@@ -4,3 +4,9 @@ class Cat(Animal):
     def speak(this):
         name = this.name
         return f"{name} meows."
+
+    
+# using proper self rather than this
+class Cat(Animal):
+    def speak(self):
+        return self.name + ' meows.'
