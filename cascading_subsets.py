7906f5db0e394f29701ecaa432bd7c0ac25de116
Greg Mahone <gmahone@gmail.com>
2023-03-19 20:51:58 +0100

    feat: add solution with single line

--- cascading_subsets.py
+++ cascading_subsets.py
@@ -3,3 +3,8 @@ def each_cons(lst, n):
     for i in range(0, len(lst) - n + 1):
         result.append(lst[i:i+n])
     return result
+
+
+# single line func
+def each_cons(lst, n):
+    return [lst[i:i+n] for i in range(len(lst) - n + 1)]
